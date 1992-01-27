4379edfdcf575314e58d18f5ea169e19845b980d
Kresten Krab Thorup <krab@trifork.com>
1992-01-27 16:12:11 +0000

    *** empty log message ***

--- auc-tex.el
+++ auc-tex.el
@@ -9,11 +9,11 @@
 ;; LCD Archive Entry:
 ;; AUC TeX|Kresten Krab Thorup|krab@iesd.auc.dk
 ;; | A much enhanced LaTeX mode 
-;; |$Date: 1992-01-27 16:11:54 $|$Revision: 5.20 $|iesd.auc.dk:/pub/emacs-lisp/auc-tex.tar.Z
+;; |$Date: 1992-01-27 16:12:11 $|$Revision: 5.21 $|iesd.auc.dk:/pub/emacs-lisp/auc-tex.tar.Z
 ;; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; RCS status      : $Revision: 5.20 $  
+;; RCS status      : $Revision: 5.21 $  
 ;; Author          : Kresten Krab Thorup
 ;; Created On      : Fri May 24 09:36:21 1991
 ;; Last Modified By: Kresten Krab Thorup
