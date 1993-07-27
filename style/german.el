b4422054cd0a05b9209729fb2350c39ea0c8abe7
Per Abrahamsen <abraham@dina.kvl.dk>
1993-07-27 18:53:36 +0000

    Make " have word syntax for germans.

--- style/german.el
+++ style/german.el
@@ -1,11 +1,18 @@
 ;;; @ german.el - Special use of double quotes.
 ;;;
-;;; $Id: german.el,v 1.2 1993-07-16 04:34:40 amanda Exp $
+;;; $Id: german.el,v 1.3 1993-07-27 18:53:36 amanda Exp $
 
 ;;; @@ Hook
 
+(defvar LaTeX-german-mode-syntax-table
+  (copy-syntax-table LaTeX-mode-syntax-table)
+  "Syntax table used in LaTeX mode when using `german.sty'.")
+
+(modify-syntax-entry ?\"  "w"  LaTeX-german-mode-syntax-table)
+
 (TeX-add-style-hook "german"
  (function (lambda ()
+   (set-syntax-table LaTeX-german-mode-syntax-table)
    (make-local-variable 'TeX-open-quote)
    (make-local-variable 'TeX-close-quote)
    (setq TeX-open-quote "\"")
