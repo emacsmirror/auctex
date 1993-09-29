5d74b82afdbf382e101a4a0b3914b499d5ca0935
Per Abrahamsen <abraham@dina.kvl.dk>
1993-09-29 14:28:19 +0000

    Fixed bugs reported by koba.

--- auc-ver.el
+++ auc-ver.el
@@ -1,5 +1,5 @@
-(defconst AUC-TeX-version "8.0f"
+(defconst AUC-TeX-version "8.0g"
   "AUC TeX version number")
-(defconst AUC-TeX-date "Wed Sep 29 00:31:54 MET 1993"
+(defconst AUC-TeX-date "Wed Sep 29 15:25:40 MET 1993"
   "AUC TeX release date")
 (provide 'auc-ver)
