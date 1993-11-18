cd1339c5bce33522c841e769d90a6b6d74ab3b17
Per Abrahamsen <abraham@dina.kvl.dk>
1993-11-18 20:43:11 +0000

    none

--- auc-ver.el
+++ auc-ver.el
@@ -1,5 +1,5 @@
 (defconst AUC-TeX-version "8.0k"
   "AUC TeX version number")
-(defconst AUC-TeX-date "Thu Nov 18 21:04:00 MET 1993"
+(defconst AUC-TeX-date "Thu Nov 18 21:30:07 MET 1993"
   "AUC TeX release date")
 (provide 'auc-ver)
