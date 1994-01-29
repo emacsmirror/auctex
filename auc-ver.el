690ece06eb8c8ef381ed7b6393f23391c9b89dcd
Per Abrahamsen <abraham@dina.kvl.dk>
1994-01-29 21:05:14 +0000

    Release 8.1c

--- auc-ver.el
+++ auc-ver.el
@@ -1,5 +1,5 @@
 (defconst AUC-TeX-version "8.1c"
   "AUC TeX version number")
-(defconst AUC-TeX-date "Sat Jan 29 21:57:00 MET 1994"
+(defconst AUC-TeX-date "Sat Jan 29 22:04:47 MET 1994"
   "AUC TeX release date")
 (provide 'auc-ver)
