beac24d921206ca780fe22b20e40b68acf7956eb
Per Abrahamsen <abraham@dina.kvl.dk>
1994-04-14 20:36:38 +0000

    Release 9.0f

--- auc-ver.el
+++ auc-ver.el
@@ -1,5 +1,5 @@
 (defconst AUC-TeX-version "9.0f"
   "AUC TeX version number")
-(defconst AUC-TeX-date "Thu Apr 14 22:16:39 MET DST 1994"
+(defconst AUC-TeX-date "Thu Apr 14 22:36:16 MET DST 1994"
   "AUC TeX release date")
 (provide 'auc-ver)
