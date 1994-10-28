400d25e1a520a6671a0f5241455e154991d4ae88
Per Abrahamsen <abraham@dina.kvl.dk>
1994-10-28 19:03:40 +0000

    Update

--- auc-menu.el
+++ auc-menu.el
@@ -1,11 +1,11 @@
 ;;; auc-menu.el - Easy menu support for FSF and Lucid Emacs 19.
 ;; 
-;; $Id: auc-menu.el,v 5.5 1994-10-25 13:18:02 amanda Exp $
+;; $Id: auc-menu.el,v 5.6 1994-10-28 19:03:40 amanda Exp $
 ;;
 ;; LCD Archive Entry:
 ;; auc-menu|Per Abrahamsen|abraham@iesd.auc.dk|
 ;; Easy menu support for FSF and Lucid Emacs 19|
-;; $Date: 1994-10-25 13:18:02 $|$Revision: 5.5 $|~/misc/auc-menu.el.gz|
+;; $Date: 1994-10-28 19:03:40 $|$Revision: 5.6 $|~/misc/auc-menu.el.gz|
 
 ;; Copyright (C) 1992, 1993, 1994 Free Software Foundation, Inc.
 ;; Copyright (C) 1994 Per Abrahamsen <abraham@iesd.auc.dk>
@@ -48,19 +48,7 @@
 ;;     DOC is the documentation string for the variable.
 ;;     MENU is a Lucid style menu description.  
 ;;
-;;     A lucid style menu is a list where the first element is
-;;     a string with the name of the menu, and the remaining elements
-;;     are the menu items.  Each item can be either a
-;;     - Menu: for nested menus.
-;;     - String: for menu items that can not be selected.
-;;     - Vector: for normal items.  It has three elements:
-;;       1. A string with the name of the menu item.
-;;       2. The function to be executed when the item is selected.
-;;          This can be either a function name or a lisp expression.
-;;       3. Normally `t', but it can be a symbol which must be
-;;          evaluate to non-nil to keep the item active, or the
-;;          speciel symbol :keys, in which case the fourth element is
-;;          a string containing the keyboard shortcut. 
+;;     See the documentation for easy-menu-define for details.
 ;;
 ;; - Function: easy-menu-change PATH NAME ITEMS
 ;;     Change an existing menu.
@@ -84,24 +72,84 @@
 ;; explicitly call `easy-menu-add' and `easy-menu-remove' to add and
 ;; remove menus from the menu bar.
 
-;; auc-menu.el defines a superset of the easymenu API included in
-;; Emacs 19.23 and later.
+;; auc-menu.el define the easymenu API included in Emacs 19.29 and
+;; later.
 
 ;;; Code:
+
+;;;###autoload
+(defmacro easy-menu-define (symbol maps doc menu)
+  "Define a menu bar submenu in maps MAPS, according to MENU.
+The arguments SYMBOL and DOC are ignored; they are present for
+compatibility only.  SYMBOL is not evaluated.  In other Emacs versions
+these arguments may be used as a variable to hold the menu data, and a
+doc string for that variable.
+
+The first element of MENU must be a string.  It is the menu bar item name.
+The rest of the elements are menu items.
+
+A menu item is usually a vector of three elements:  [NAME CALLBACK ENABLE]
+
+NAME is a string--the menu item name.
+
+CALLBACK is a command to run when the item is chosen,
+or a list to evaluate when the item is chosen.
+
+ENABLE is an expression; the item is enabled for selection
+whenever this expression's value is non-nil.
+
+Alternatively, a menu item may have the form: 
+
+   [ NAME CALLBACK [ KEYWORD ARG ] ... ]
+
+Where KEYWORD is one of the symbol defined below.
+
+   :keys KEYS
+
+KEYS is a string; a complex keyboard equivalent to this menu item.
+
+   :active ENABLE
+
+ENABLE is an expression; the item is enabled for selection
+whenever this expression's value is non-nil.
+
+   :suffix NAME
+
+NAME is a string; the name of an argument to CALLBACK.
+
+   :style STYLE
+   
+STYLE is a symbol describing the type of menu item.  The following are
+defined:  
+
+toggle: A checkbox.  
+        Currently just prepend the name with the string \"Toggle \".
+radio: A radio button. 
+nil: An ordinary menu item.
+
+   :selected SELECTED
+
+SELECTED is an expression; the checkbox or radio button is selected
+whenever this expression's value is non-nil.
+Currently just disable radio buttons, no effect on checkboxes.
+
+A menu item can be a string.  Then that string appears in the menu as
+unselectable text.  A string consisting solely of hyphens is displayed
+as a solid horizontal line.
+
+A menu item can be a list.  It is treated as a submenu.
+The first element should be the submenu name.  That's used as the
+menu item in the top-level menu.  The cdr of the submenu list
+is a list of menu items, as above."
+  (` (progn
+       (defvar (, symbol) nil (, doc))
+       (easy-menu-do-define (quote (, symbol)) (, maps) (, doc) (, menu)))))
 
 (cond 
 
 ;;; Emacs 18
 
 ((< (string-to-int emacs-version) 19)
-
-(defmacro easy-menu-define (symbol maps doc menu)
-  "Define SYMBOL to be a menu for keymaps MAPS.
-DOC is the documentation string, and MENU is a Lucid style menu."
-  (` (progn
-       (require 'auc-menu)		;For `easy-menu-do-define'.
-       (defvar (, symbol) nil (, doc))
-       (easy-menu-do-define (quote (, symbol)) (, maps) (, doc) (, menu)))))
 
 (defun easy-menu-do-define (symbol maps doc menu)
   (fset symbol (symbol-function 'ignore)))
@@ -112,21 +160,11 @@ DOC is the documentation string, and MENU is a Lucid style menu."
 
 (defun easy-menu-change (path name items))
 
-(provide 'easymenu)
-
 )					;Emacs 18
 
 ;;; Lucid Emacs
 
 ((string-match "Lucid" emacs-version)
-
-(defmacro easy-menu-define (symbol maps doc menu)
-  "Define SYMBOL to be a menu for keymaps MAPS.
-DOC is the documentation string, and MENU is a Lucid style menu."
-  (` (progn
-       (require 'auc-menu)		;For `easy-menu-do-define'.
-       (defvar (, symbol) nil (, doc))
-       (easy-menu-do-define (quote (, symbol)) (, maps) (, doc) (, menu)))))
 
 (defun easy-menu-do-define (symbol maps doc menu)
   (set symbol menu)
@@ -163,20 +201,118 @@ DOC is the documentation string, and MENU is a Lucid style menu."
        (assoc (car menu) current-menubar)
        (delete-menu-item (list (car menu)))))
 
-(provide 'easymenu)
-
 )					;Lucid Emacs
 
 ;;; FSF Emacs 19
 
 (t
 
-(require 'easymenu)
+(defun easy-menu-do-define (symbol maps doc menu)
+  ;; We can't do anything that might differ between Emacs dialects in
+  ;; `easy-menu-define' in order to make byte compiled files
+  ;; compatible.  Therefore everything interesting is done in this
+  ;; function. 
+  (set symbol (easy-menu-create-keymaps (car menu) (cdr menu)))
+  (fset symbol (` (lambda (event) (, doc) (interactive "@e")
+		    (easy-popup-menu event (, symbol)))))
+  (mapcar (function (lambda (map) 
+	    (define-key map (vector 'menu-bar (intern (car menu)))
+	      (cons (car menu) (symbol-value symbol)))))
+	  (if (keymapp maps) (list maps) maps)))
+
+(defvar easy-menu-item-count 0)
+
+;; Return a menu keymap corresponding to a Lucid-style menu list
+;; MENU-ITEMS, and with name MENU-NAME.
+(defun easy-menu-create-keymaps (menu-name menu-items)
+  (let ((menu (make-sparse-keymap menu-name)))
+    ;; Process items in reverse order,
+    ;; since the define-key loop reverses them again.
+    (setq menu-items (reverse menu-items))
+    (while menu-items
+      (let* ((item (car menu-items))
+	     (callback (if (vectorp item) (aref item 1)))
+	     command enabler name)
+	(cond ((stringp item)
+	       (setq command nil)
+	       (setq name (if (string-match "^-+$" item) "" item)))
+	      ((consp item)
+	       (setq command (easy-menu-create-keymaps (car item) (cdr item)))
+	       (setq name (car item)))
+	      ((vectorp item)
+	       (setq command (make-symbol (format "menu-function-%d"
+						  easy-menu-item-count)))
+	       (setq easy-menu-item-count (1+ easy-menu-item-count))
+	       (setq name (aref item 0))
+	       (let ((keyword (aref item 2)))
+		 (if (and (symbolp keyword)
+			  (= ?: (aref (symbol-name keyword) 0)))
+		     (let ((count 2)
+			   style selected active keys
+			   arg)
+		       (while (> (length item) count)
+			 (setq keyword (aref item count))
+			 (setq arg (aref item (1+ count)))
+			 (setq count (+ 2 count))
+			 (cond ((eq keyword ':keys)
+				(setq keys arg))
+			       ((eq keyword ':active)
+				(setq active arg))
+			       ((eq keyword ':suffix)
+				(setq name (concat name " " arg)))
+			       ((eq keyword ':style)
+				(setq style arg))
+			       ((eq keyword ':selected)
+				(setq selected arg))))
+		       (if keys
+			   (setq name (concat name "  (" keys ")")))
+		       (if (eq style 'toggle)
+			   ;; Simulate checkboxes.
+			   (setq name (concat "Toggle " name)))
+		       (if active 
+			   (put command 'menu-enable active)
+			 (and (eq style 'radio)
+			      selected
+			      ;; Simulate radio buttons with menu-enable.
+			      (put command 'menu-enable
+				   (list 'not selected)))))))	       
+	       (if (keymapp callback)
+		   (setq name (concat name " ...")))
+	       (if (symbolp callback)
+		   (fset command callback)
+		 (fset command (list 'lambda () '(interactive) callback)))))
+	(if (null command)
+	    ;; Handle inactive strings specially--allow any number
+	    ;; of identical ones.
+	    (setcdr menu (cons (list nil name) (cdr menu)))
+	  (if name 
+	      (define-key menu (vector (intern name)) (cons name command)))))
+      (setq menu-items (cdr menu-items)))
+    menu))
+
+(defun easy-menu-change (path name items)
+  "Change menu found at PATH as item NAME to contain ITEMS.
+PATH is a list of strings for locating the menu containing NAME in the
+menu bar.  ITEMS is a list of menu items, as in `easy-menu-define'.
+These items entirely replace the previous items in that map.
+
+Call this from `activate-menubar-hook' to implement dynamic menus."
+  (let ((map (key-binding (apply 'vector
+				 'menu-bar
+				 (mapcar 'intern (append path (list name)))))))
+    (if (keymapp map)
+	(setcdr map (cdr (easy-menu-create-keymaps name items)))
+      (error "Malformed menu in `easy-menu-change'"))))
+
+(defun easy-menu-remove (menu))
+
+(defun easy-menu-add (menu &optional map))
 
 )					;FSF Emacs 19
 
 )					;cond
 
+(provide 'easymenu)
 (provide 'auc-menu)
 
 ;;; auc-menu.el ends here
