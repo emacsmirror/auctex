4204de8c02b33c775bc6f6ea2cf0cc7a4d12f959
Per Abrahamsen <abraham@dina.kvl.dk>
1997-01-17 16:42:23 +0000

    Release 9.7c++

--- tex.el
+++ tex.el
@@ -1,7 +1,7 @@
 ;;; tex.el --- Support for TeX documents.
 
 ;; Maintainer: Per Abrahamsen <auc-tex@sunsite.auc.dk>
-;; Version: 9.7c
+;; Version: 9.7d
 ;; Keywords: wp
 ;; X-URL: http://sunsite.auc.dk/auctex
 
