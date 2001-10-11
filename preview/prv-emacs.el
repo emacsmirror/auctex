e4f9798cb7858ff02f26565084ee28b662d039b2
David Kastrup <dak@gnu.org>
2001-10-11 15:33:49 +0000

    Empty prv-emacs.el submitted so that configure/make can start dealing with it without breaking the current workability of preview-latex.

---
+++ preview/prv-emacs.el
@@ -0,0 +1,30 @@
+;;; prv-emacs.el --- GNU Emacs specific code for preview.el
+
+;; Copyright (C) 2001  Free Software Foundation, Inc.
+
+;; Author: David Kastrup <David.Kastrup@t-online.de>
+;; Keywords: convenience, tex, wp
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+;;; prv-emacs.el ends here
