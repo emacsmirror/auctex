acd3c32586d7bffc003e33cf1184dade48018ec0
arafune <arafune>
2001-10-17 10:19:44 +0000

    *** empty log message ***

--- style/graphicx.el
+++ style/graphicx.el
@@ -3,9 +3,9 @@
 
 ;; Copyright (C) 2000 by Free Software Foundation, Inc.
 
-;; Author: Ryuichi Arafune <arafune@ushioda.riec.tohoku.ac.jp>
+;; Author: Ryuichi Arafune <arafune@debian.org>
 ;; Created: 1999/3/20
-;; Version: $Id: graphicx.el,v 1.3 2001-10-03 15:48:05 abraham Exp $
+;; Version: $Id: graphicx.el,v 1.4 2001-10-17 10:19:44 arafune Exp $
 ;; Keywords: tex
 
 ;; This file is free software; you can redistribute it and/or modify
@@ -23,98 +23,86 @@
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-;;; Commentary:
+;;; Commentary: 
 ;;  This package suppors the includegraphcics macro in graphicx style (LaTeX2e)
 ;;  If you want to use bb, angle or totalheight as arguments of includegraphics,
 ;;  set TeX-include-graphics-simple nil (default t).
 
 ;; Acknowledgements
 ;;  Dr. Thomas Baumann <thomas.baumann@ch.tum.de>
+;;  David Kastrup <David.Kastrup@t-online.de>
 ;;
 ;;; Code:
 
-(TeX-add-style-hook
+(TeX-add-style-hook 
  "graphicx"
- (function (lambda ()
+ (function (lambda () 
 	     (TeX-add-symbols
-	      "protect" "clip" "keepaspectratio" "width" "height" "bb" "angle" "totalheight"
+	      "protect" "clip" "keepaspectratio" 
+	      "width" "height" "bb" "angle" "totalheight"
 	      '("includegraphics" TeX-arg-includegraphics)))))
 
-
 (defvar TeX-include-graphics-simple t
-  "if nil, AUC TeX asks the following arguments: Bounding box (bb), Rotation angle (angle), Total height (totalheight) in addition to normal arguments.")
+  "if nil, AUC TeX asks the following arguments: Bounding box (bb), Rotation angle (angle), Total height (totalheight) in addition to the normal arguments.")
 
 (defun TeX-arg-includegraphics (optional)
   "Ask for file name (eps file only), width, height, keepaspectratio, and clip. Insert includegraphics macro"
-  (let ((width-flag nil) (height-flag nil) (left-brace-flag nil)
-	(psfile
-	 (completing-read "PS (eps only) file: "
+  (let ((maybe-left-brace "[") (maybe-comma "")
+	(psfile 
+	 (completing-read "PS (eps only) file: " 
 			  (mapcar 'list (directory-files "./" nil "\\.eps$" nil))
-			  nil t
+			  nil nil
 			  (car (car (mapcar 'list (directory-files "./" nil "\\.eps$" nil))))))
 	(figwidth (read-input "Figure width (cm): "))
 	(figheight (read-input "Figure height (cm): "))
 	(keepaspectratio (y-or-n-p "Keep Aspectratio ? "))
 	(clip (y-or-n-p "Clipping figure ? ")))
-    (if (or (not (zerop (length figwidth))) (not (zerop (length figheight))) keepaspectratio clip)
-	(progn (insert "[") (setq left-brace-flag t)))
-    (if (not (zerop (length figwidth)))
-	(progn 
-	  (if (TeX-string-numberp figwidth)
-	      (insert "width="figwidth"cm")
-	    (insert "width="figwidth))
-	  (setq width-flag t)))
-    (if (and (not (zerop (length figheight))) width-flag)
-	(progn 
-	  (if (TeX-string-numberp figheight)
-	      (insert ",height="figheight"cm")
-	    (insert ",height="figheight))
-	  (setq height-flag t))
-      (if (not (zerop (length figheight)))
-	  (progn
-	    (if (TeX-string-numberp figheight)
-		(insert "height="figheight"cm")
-	      (insert "height="figheight))
-	    (setq height-flag t))))
-    (if (not (and height-flag width-flag))
-	(if (and keepaspectratio (or width-flag height-flag))
-	    (insert ",keepaspectratio")
-	  (if  keepaspectratio
-	      (insert "keepaspectratio"))))
-    (if (and clip (or width-flag height-flag keepaspectratio))
-	(insert ",clip")
-      (if clip
-	  (insert "clip")))
+    (when (not (zerop (length figwidth)))
+      (if (TeX-string-numberp figwidth)
+	  (insert maybe-left-brace "width=" figwidth "cm")
+	(insert maybe-left-brace "width=" figwidth))
+      (setq maybe-comma ",")
+      (setq maybe-left-brace ""))
+    (when (not (zerop (length figheight)))
+      (if (TeX-string-numberp figheight)
+	  (insert maybe-left-brace "height=" figheight "cm")
+	(insert maybe-left-brace "height=" figheight))
+      (setq maybe-comma ",")
+      (setq maybe-left-brace ""))
+    (when keepaspectratio
+      (insert maybe-left-brace maybe-comma "keepaspectratio")
+      (setq maybe-comma ",")
+      (setq maybe-left-brace ""))
+    (when clip
+      (insert maybe-left-brace maybe-comma "clip")
+      (setq maybe-comma ",")
+      (setq maybe-left-brace ""))
 ;;; Insert more arguments when TeX-include-graphics-simple is nil
     (if (not TeX-include-graphics-simple)
-	(progn
-	  (let ((bbset-flag (y-or-n-p "Set Bounding Box :"))
-		(bbllx nil) (bblly nil) (bburx nil) (bbury nil)	(angle-flag nil)
-		(angle "") (totalheight ""))
-	    (if bbset-flag
-		(progn (setq  bbllx (read-input "Bounding Box Lower Left x :"))
-		       (setq  bblly (read-input "Bounding Box Lower Left y :"))
-		       (setq  bburx (read-input "Bounding Upper right x :"))
-		       (setq  bbury (read-input "Bounding Upper right y :"))))
-	    (if (and bbset-flag (or clip keepaspectratio width-flag height-flag))
-		(insert ",bb="bbllx bblly bburx bbury)
-	      (if bbset-flag
-		  (insert "bb="bbllx bblly bburx bbury)))
-	    (setq angle (read-input "Rotation Angle :"))
-	    (if (not (zerop (length angle)))
-		(progn (setq angle-flag t)
-		       (if (or clip keepaspectratio width-flag height-flag bbset-flag)
-			   (insert ",angle="angle)
-			 (if angle-flag
-			     (insert "angle="angle)))))
-	    (setq totalheight (read-input "Total Height (cm):"))
-	    (if (not (zerop (length totalheight)))
-		(if (or clip keepaspectratio width-flag height-flag bbset-flag angle-flag)
-		    (insert ",totalheight="totalheight)
-		  (if (not (zerop (length totalheight)))
-		      (insert "totalheight="totalheight)))))))
+	(let ((angle (read-input "Rotation angle: "))
+	      (totalheight (read-input "Total Height (cm): "))
+	      (bbset-flag (y-or-n-p "Set Bounding Box : "))
+	      (bbllx nil) (bblly nil) (bburx nil) (bbury nil))
+	  (when bbset-flag
+	    (setq bbllx (read-input "Bounding Box Lower Left x: "))
+	    (setq bblly (read-input "Bounding Box Lower Left y: "))
+	    (setq bburx (read-input "Bounding Box Upper right x: "))
+	    (setq bbury (read-input "Bounding Box Upper right y: "))
+	    (insert maybe-left-brace maybe-comma 
+		    "bb=" bbllx " " bblly " " bburx " " bbury)
+	    (setq maybe-comma ",")
+	    (setq maybe-left-brace ""))
+	  (when (not (zerop (length angle)))
+	    (insert maybe-left-brace maybe-comma "angle=" angle)
+	    (setq maybe-comma ",")
+	    (setq maybe-left-brace ""))
+	  (when (not (zerop (length totalheight)))
+	    (if (TeX-string-numberp totalheight)
+		(insert maybe-left-brace maybe-comma "totalheight=" totalheight "cm")
+	      (insert maybe-left-brace maybe-comma "totalheight=" totalheight))
+	    (setq maybe-left-brace ""))))
 ;;;
-    (if left-brace-flag
+    (if (zerop (length maybe-left-brace))
 	(insert "]"))
     (TeX-insert-braces 0)
     (insert psfile)
@@ -123,5 +111,6 @@
 (defun TeX-string-numberp (string)
   (if (string-match "[0-9]*\\.?[0-9]+" string)
       (not (string-match "[a-zA-Z]" string))))
+
 
 ;;; graphicx.el ends here
