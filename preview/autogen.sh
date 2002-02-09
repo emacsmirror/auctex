0c6ff9f43e7e747420a6cb183d08cfdfda75c7ed
David Kastrup <dak@gnu.org>
2002-02-09 22:33:08 +0000

    Initial version of autogen.sh Should generate configure, PROBLEMS, README, INSTALL.

---
+++ preview/autogen.sh
@@ -0,0 +1,5 @@
+:
+autoconf
+makeinfo --no-headers --no-validate doc/readme.texi >README
+makeinfo --no-headers --no-validate doc/install.texi >INSTALL
+makeinfo --no-headers --no-validate doc/problems.texi >PROBLEMS
