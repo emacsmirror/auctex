91c55f94a6030b548b6e1c2fd9c5be53c3061d40
David Kastrup <dak@gnu.org>
2002-03-04 02:13:12 +0000

    Pregenerate info file.

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -3,3 +3,4 @@ autoconf
 makeinfo --no-headers --no-validate doc/readme.texi >README
 makeinfo --no-headers --no-validate doc/install.texi >INSTALL
 makeinfo --no-headers --no-validate doc/problems.texi >PROBLEMS
+(cd doc;makeinfo preview-latex.texi)
