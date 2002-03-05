0efc298ea5ea8569215074d793ee812ce61241cd
David Kastrup <dak@gnu.org>
2002-03-05 14:19:56 +0000

    Wheedle MY_LANG and MY_LC_CTYPE in if autoconf failed to do so.

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -1,5 +1,16 @@
-:
+#!/bin/sh
 autoconf
+grep -q '^MY_LANG=' configure || {
+  echo "Trying to deal with outdated autoconf"
+  ed -s configure <<\EOF
+/^[^#]/i
+MY_LANG=$LANG
+MY_LC_CTYPE=$LC_CTYPE
+.
+w
+q
+EOF
+}
 makeinfo --no-headers --no-validate doc/readme.texi >README
 makeinfo --no-headers --no-validate doc/install.texi >INSTALL
 makeinfo --no-headers --no-validate doc/problems.texi >PROBLEMS
