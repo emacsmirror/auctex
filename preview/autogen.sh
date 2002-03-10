6ab597ff85768c18755b6f959198041bfff70ee0
David Kastrup <dak@gnu.org>
2002-03-10 14:08:13 +0000

    don't prebuild doc.  We offer a separate download archive for people without texinfo.

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -14,4 +14,3 @@ EOF
 makeinfo --no-headers --no-validate doc/readme.texi >README
 makeinfo --no-headers --no-validate doc/install.texi >INSTALL
 makeinfo --no-headers --no-validate doc/problems.texi >PROBLEMS
-(cd doc;makeinfo preview-latex.texi)
