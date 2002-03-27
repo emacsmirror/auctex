530b1c09a030662f822dcb6342380d415812e97e
Alan Shutko <ats@acm.org>
2002-03-27 04:13:55 +0000

    * doc/.cvsignore: Add preview-latex.tmp.
    * .cvsignore: Add latex.out.
    
    * autogen.sh: Modify ed script to account for difference between
    AC_INIT in 2.13 and 2.52
    
    * doc/Makefile.in (INSTALLINFO): Make it use configure value.
    
    * configure.in: Check for path of install-info.  Check whether
    Emacs supports images.

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -2,11 +2,14 @@
 autoconf
 grep -q '^MY_LANG=' configure || {
   echo "Trying to deal with outdated autoconf"
+# Two fixes: Save LANG and LC_CTYPE before configure munges it
+# Account for difference between AC_INIT in 2.13 and 2.52
   ed -s configure <<\EOF
 /^[^#]/i
 MY_LANG=$LANG
 MY_LC_CTYPE=$LC_CTYPE
 .
+,s/ac_unique_file=preview-latex/ac_unique_file=preview-latex.el/
 w
 q
 EOF
