9c786f21ef5d425b08cb8068db5b05f466fb738f
David Kastrup <dak@gnu.org>
2002-04-20 18:01:05 +0000

    (preview-latex.el): Autogenerated.

--- preview/preview-latex.el
+++
@@ -1,5 +0,0 @@
-;; This file contains the autoload of preview-latex
-;;
-;; The ideal place for this file is in the `site-lisp.d' directory.
-(autoload 'LaTeX-preview-setup "preview")
-(add-hook 'LaTeX-mode-hook #'LaTeX-preview-setup)
