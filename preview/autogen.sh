b7bfe1bd26b44cc9a4dad06171d1cb0cb367a4b7
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2002-12-10 11:26:14 +0000

    Make LANG and LC_CTYPE saving work across recursive call of ./configure (2.13)

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -4,13 +4,16 @@ grep -q '^MY_LANG=' configure || {
   echo "Using an old autoconf, are we?"
   echo "Fixing the two known issues now."
 # Two fixes: Save LANG and LC_CTYPE before configure munges it
+# even across recursive call(s) of ./configure
 # Account for difference between AC_INIT in 2.13 and 2.52
   ed -s configure <<\EOF
 /^[^#]/i
-MY_LANG=$LANG
-MY_LC_CTYPE=$LC_CTYPE
-MY_LANGUAGE=$LANGUAGE
-MY_LC_ALL=$LC_ALL
+if test -z "$MY_LANG$MY_LC_CTYPE$MY_LANGUAGE$MY_LC_ALL"; then 
+  MY_LANG=$LANG; export MY_LANG
+  MY_LC_CTYPE=$LC_CTYPE; export MY_LC_CTYPE
+  MY_LANGUAGE=$LANGUAGE; export MY_LANGUAGE
+  MY_LC_ALL=$LC_ALL; export MY_LC_ALL
+fi
 .
 ,s/ac_unique_file=preview-latex/ac_unique_file=preview.el/
 w
