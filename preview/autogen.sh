db73bb9f5292fee7edcf7cf4fa086b4e6f65c9b5
David Kastrup <dak@gnu.org>
2003-01-07 09:47:05 +0000

    Make much more simple, so that this will work with more sed versions.

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -10,11 +10,11 @@ echo "Patching around autoconf issues now."
 # Two fixes: Save LANG and LC_CTYPE before configure munges it
 # even across recursive call(s) of ./configure
 # Account for difference between AC_INIT in 2.13 and 2.52
-sed '2i \
-'"`sed -n 's/^#autogen.sh: //p' configure.in | sed 's/\$/ \\\\/'`"'
-
-s/ac_unique_file=preview-latex/ac_unique_file=preview.el/' \
-  configure >configure.sed && cat configure.sed >configure && rm configure.sed
+sed -n '1p' configure >configure.sed
+sed -n 's/^#autogen.sh: //p' configure.in >>configure.sed
+sed '1d;s/ac_unique_file=preview-latex/ac_unique_file=preview.el/' \
+  configure >>configure.sed
+cat configure.sed >configure && rm configure.sed
 
 cd doc
 makeinfo -D rawfile --no-headers --no-validate readme.texi >../README
