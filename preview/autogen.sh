ddeb66dd80503ec8670b5f5069091b008aeea55c
David Kastrup <dak@gnu.org>
2003-01-10 00:11:44 +0000

    Ok, darn them all.  We now have a configure.in that does no longer need to get patched up after autoconf, regardless of which autoconf version gets used between 2.13 and 2.57 or hopefully later.

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
-rm -f configure
-
 if test -z "$AUTOCONF"
 then autoconf
 else $AUTOCONF
 fi
-
-echo "Patching around autoconf issues now."
-# Two fixes: Save LANG and LC_CTYPE before configure munges it
-# even across recursive call(s) of ./configure
-# Account for difference between AC_INIT in 2.13 and 2.52
-sed -n '1p' configure >configure.sed
-sed -n 's/^#autogen.sh: //p' configure.in >>configure.sed
-sed '1d;s/ac_unique_file=preview-latex/ac_unique_file=preview.el/' \
-  configure >>configure.sed
-cat configure.sed >configure && rm configure.sed
 
 cd doc
 makeinfo -D rawfile --no-headers --no-validate readme.texi >../README
