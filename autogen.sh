f8706e1132244e7c600367eee2eb3cd2d3583fa2
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2003-01-10 07:53:42 +0000

    Currently not needed. Perhaps for README and suchlike later.

--- autogen.sh
+++ autogen.sh
@@ -4,9 +4,5 @@ then autoconf
 else $AUTOCONF
 fi
 
-echo "Patching around autoconf issues now."
-# Account for difference between AC_INIT in 2.13 and 2.52
-sed 's/ac_unique_file=auc-tex/ac_unique_file=auctex.spec/' \
-  configure >>configure.sed
-cat configure.sed >configure && rm configure.sed
+
 
