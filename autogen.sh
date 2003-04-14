03a0bd5f2612efd22b0ea26db07fcdd8e1de4f5d
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2003-04-14 12:14:49 +0000

    Generate INSTALL and INSTALL.windows

--- autogen.sh
+++ autogen.sh
@@ -4,5 +4,13 @@ then autoconf
 else $AUTOCONF
 fi
 
+cd doc
+#makeinfo -D rawfile --no-headers --no-validate readme.texi >../README
+makeinfo -D rawfile --no-headers --no-validate install.texi >../INSTALL
+#makeinfo -D rawfile --no-headers --no-validate problems.texi >../PROBLEMS
+#makeinfo -D rawfile --no-headers --no-validate todo.texi >../TODO
+makeinfo -D rawfile --no-headers --no-validate wininstall.texi >../INSTALL.windows
+#makeinfo -D rawfile --no-headers --no-validate --number-sections faq.texi >../FAQ
+cd ..
 
 
