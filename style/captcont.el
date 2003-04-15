bef47fc014bc5dbb5dc3f2e6014ce3481e7042db
Reiner Steib <reiner.steib@gmx.de>
2003-04-15 16:23:06 +0000

    (STYLESRC): Added captcont.el and subfigure.el

---
+++ style/captcont.el
@@ -0,0 +1,22 @@
+;; captcont.el --- AUCTeX style file for captcont.sty
+;; Copyright (C) 2003 Reiner Steib
+
+;; Author: Reiner Steib  <Reiner.Steib@gmx.de>
+;; Keywords: tex
+
+;;; Commentary:
+;;
+;; AUCTeX style file for captcont.sty
+
+;;; Code:
+
+(TeX-add-style-hook
+ "captcont"
+ (lambda ()
+   (TeX-add-symbols
+    '("captcont"  [ "list entry" ] "Caption")
+    '("captcont*" [ "list entry" ] "Caption"))
+   (add-to-list 'font-latex-match-textual-keywords-local "captcont")
+   (font-latex-match-textual-make)))
+
+;;; captcont.el ends here
