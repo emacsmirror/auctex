63b77dc677c6783300b8645e037c784521c4f75e
Ralf Angeli <angeli@caeruleus.net>
2004-01-12 18:50:15 +0000

    (plain-TeX-mode-command-menu, LaTeX-mode-command-menu): Use new constant `TeX-command-menu-name' and call `TeX-mode-specific-command-menu' via `:filter'.

--- tex-jp.el
+++ tex-jp.el
@@ -53,12 +53,18 @@
 (easy-menu-define plain-TeX-mode-command-menu
     plain-TeX-mode-map
     "Command menu used in TeX mode."
-    (TeX-mode-specific-command-menu 'plain-tex-mode))
+    (list TeX-command-menu-name
+          :filter (lambda (&rest ignored)
+                    (TeX-mode-specific-command-menu 'plain-tex-mode))
+          "Bug."))
 
 (easy-menu-define LaTeX-mode-command-menu
     LaTeX-mode-map
     "Command menu used in LaTeX mode."
-    (TeX-mode-specific-command-menu 'latex-mode))
+    (list TeX-command-menu-name
+          :filter (lambda (&rest ignored)
+                    (TeX-mode-specific-command-menu 'latex-mode))
+          "Bug."))
 
 
 (setq LaTeX-command-style
