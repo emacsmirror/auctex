20d2e51d1bb5f7840dd9276ae38d3c6c395a0f11
Ralf Angeli <angeli@caeruleus.net>
2004-01-12 18:51:37 +0000

    (LaTeX-mode-command-menu): Use new constant `TeX-command-menu-name' and call `TeX-mode-specific-command-menu' via `:filter'.

--- latex.el
+++ latex.el
@@ -2919,7 +2919,10 @@ the last entry in the menu."
 (easy-menu-define LaTeX-mode-command-menu
     LaTeX-mode-map
     "Command menu used in LaTeX mode."
-    (TeX-mode-specific-command-menu 'latex-mode))
+    (list TeX-command-menu-name
+          :filter (lambda (&rest ignored)
+                    (TeX-mode-specific-command-menu 'latex-mode))
+          "Bug."))
 
 (easy-menu-define LaTeX-mode-menu
     LaTeX-mode-map
