b4b96044ef305d768edf4b843a07e27799b58fd7
David Kastrup <dak@gnu.org>
2004-04-24 21:42:03 +0000

    ("natbib"): Add fontification for regexps. Patch by Thomas Baumann.

--- style/natbib.el
+++ style/natbib.el
@@ -66,7 +66,16 @@
 			   citecmds "\\|")
 		"\\)\\(\\[[^]\n\r\\%]*\\]\\)*{\\([^{}\n\r\\%,]*,\\)*\\([^{}\n\r\\%,]*\\)")
 	4 'LaTeX-bibitem-list "}")))
-    
+
+    ;; Add fontification for references
+    (setq font-latex-match-reference-keywords-local
+          (append font-latex-match-reference-keywords-local
+            '("cite" "citet" "citet*" "citealt" "citealt*"
+              "citep" "citep*" "citealp" "citealp*" 
+              "citeauthor" "citeauthor*" "citefullauthor"
+              "citeyear" "citeyearpar""shortcites")))
+    (font-latex-match-reference-make)
+
     ;; Tell RefTeX
     (if (fboundp 'reftex-set-cite-format)
 	(reftex-set-cite-format 'natbib)))))
