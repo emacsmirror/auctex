58d48bb38eb5ec3aa258175447c0c49aa98f86cb
Reiner Steib <reiner.steib@gmx.de>
2004-05-05 12:02:12 +0000

    * Makefile.in (tar-ball-clean): New.
    * autogen.sh: Create README and TODO.
    
    * .cvsignore, doc/.cvsignore: Add TODO.

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,12 @@ else $AUTOCONF
 fi
 
 cd doc
-#makeinfo -D rawfile --no-headers --no-validate readme.texi >../README
+# We don't want to rename intro.texi to readme.texi
+# in order to keep CVS history.
+makeinfo -D rawfile --no-headers --no-validate intro.texi >../README
 makeinfo -D rawfile --no-headers --no-validate install.texi >../INSTALL
 #makeinfo -D rawfile --no-headers --no-validate problems.texi >../PROBLEMS
-#makeinfo -D rawfile --no-headers --no-validate todo.texi >../TODO
+makeinfo -D rawfile --no-headers --no-validate todo.texi >../TODO
 makeinfo -D rawfile --no-headers --no-validate wininstall.texi >../INSTALL.windows
 #makeinfo -D rawfile --no-headers --no-validate --number-sections faq.texi >../FAQ
 cd ..
