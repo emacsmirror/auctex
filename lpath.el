3707b050f154f57010cae2e7f026328161dfb5d6
Davide Giovanni Maria Salvetti <salve@debian.org>
2004-05-13 08:32:28 +0000

    Bind `TeX-auto-global'. Bind `TeX-macro-global'.

--- lpath.el
+++ lpath.el
@@ -4,5 +4,6 @@
 ;; Make sure we get the right files.
 (setq load-path (cons "." load-path)
       byte-compile-warnings nil
-      TeX-lisp-directory "<none>")
-
+      TeX-lisp-directory "<none>"
+      TeX-auto-global "<none>"
+      TeX-macro-global nil)
