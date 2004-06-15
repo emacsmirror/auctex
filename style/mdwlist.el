7e69ad6e247bd1ad5ae49ba24f2cee8684b360a6
Ralf Angeli <angeli@caeruleus.net>
2004-06-15 09:14:13 +0000

    New header. (TeX-add-style-hook): Add support for \makecompactlist, \suspend, and \resume.

--- style/mdwlist.el
+++ style/mdwlist.el
@@ -1,13 +1,53 @@
-;;; style/mdwlist.el
-;;; AUCTeX supoort for \usepackage{mdwlist}
+;;; mdwlist.el --- AUCTeX style for `mdwlist.sty'
 
-(TeX-add-style-hook "mdwlist"
-   (lambda ()
-     
-     (LaTeX-add-environments
-      '("enumerate*" LaTeX-env-item)
-      '("itemize*" LaTeX-env-item)
-      '("description*" LaTeX-env-item)
-      )
+;; Copyright (C) 2004 Free Software Foundation, Inc.
 
-))
+;; Maintainer: auc-tex@sunsite.dk
+;; Keywords: tex
+
+;; This file is part of AUCTeX.
+
+;; AUCTeX is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; AUCTeX is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with AUCTeX; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;; 02111-1307, USA.
+
+;;; Commentary:
+
+;; This file adds support for `mdwlist.sty'.
+
+;;; Code:
+
+(TeX-add-style-hook
+ "mdwlist"
+ (lambda ()
+   (TeX-add-symbols
+    '("makecompactlist" "New environment" "Existing environment")
+    '("suspend" "Environment") ; this could be done nicer by automatically
+    '("resume" "Environment")) ; determining the environment
+   (LaTeX-add-environments
+    '("enumerate*" LaTeX-env-item)
+    '("itemize*" LaTeX-env-item)
+    '("description*" LaTeX-env-item))
+   ;; Indentation and filling
+   (setq LaTeX-begin-regexp (concat LaTeX-begin-regexp "\\|resume\\b"))
+   (setq LaTeX-end-regexp (concat LaTeX-end-regexp "\\|suspend\\b"))
+   (setq paragraph-start (concat paragraph-start
+				 "\\|[ \t]*" comment-start "*[ \t]*"
+				 (regexp-quote TeX-esc)
+				 "\\(resume\\b\\|suspend\\b\\)"))
+   ;; Fontification
+   (setq font-latex-match-function-keywords-local
+         (append font-latex-match-function-keywords-local
+                 '("makecompactlist" "suspend" "resume")))
+   (font-latex-match-function-make)))
