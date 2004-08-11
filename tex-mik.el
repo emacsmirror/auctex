7a820978bbf8ee3cc769e765bbcac4fd1db07e85
Ralf Angeli <angeli@caeruleus.net>
2004-08-11 10:16:54 +0000

    Synchronize `TeX-command-list' with tex.el. Use gsview32 in `TeX-output-view-style'.

--- tex-mik.el
+++ tex-mik.el
@@ -32,17 +32,39 @@
 
 (unless (get 'TeX-command-list 'saved-value)
   (setq TeX-command-list
-	(list (list "TeX" "%(PDF)tex %S \\nonstopmode\\input %t" 'TeX-run-TeX nil
-		    (list 'plain-tex-mode))
-	      (list "LaTeX" "%l \\nonstopmode\\input{%t}" 'TeX-run-TeX nil
-		    (list 'latex-mode))
-	      (list "View" "%v" 'TeX-run-discard nil t)
-	      (list "Print" "gsview32 %f" 'TeX-run-command t t)
-	      (list "File" "dvips %d -o %f " 'TeX-run-command t t)
-	      (list "BibTeX" "bibtex %s" 'TeX-run-BibTeX nil t)
-	      (list "Index" "makeindex %s" 'TeX-run-command nil t)
-	      (list "Check" "lacheck %s" 'TeX-run-compile nil t)
-	      (list "Other" "" 'TeX-run-command t t))))
+	(list (list "TeX" "%(PDF)tex %S%(PDFout) \"%(mode)\\input %t\""
+		    'TeX-run-TeX nil (list 'plain-tex-mode)
+		    :help "Run plain TeX")
+	      (list "LaTeX" "%l \"%(mode)\\input{%t}\"" 'TeX-run-TeX nil
+		    (list 'latex-mode 'doctex-mode) :help "Run LaTeX")
+	      (list "Makeinfo" "makeinfo %t" 'TeX-run-compile nil
+		    (list 'texinfo-mode) :help "Run Makeinfo with Info output")
+	      (list "Makeinfo HTML" "makeinfo --html %t" 'TeX-run-compile nil
+		    (list 'texinfo-mode) :help "Run Makeinfo with HTML output")
+	      (list "AmSTeX" "amstex %S \"%(mode)\\input %t\""
+		    'TeX-run-TeX nil (list 'ams-tex-mode) :help "Run AMSTeX")
+	      (list "ConTeXt" "texexec --once --texutil %(execmode)%t"
+		    'TeX-run-TeX nil (list 'context-mode)
+		    :help "Run ConTeXt once")
+	      (list "ConTeXt Full" "texexec %(execmode)%t" 'TeX-run-TeX nil
+		    (list 'context-mode) :help "Run ConTeXt until completion")
+	      (list "ConTeXt Clean" "texutil --purgeall"
+		    'TeX-run-interactive nil (list 'context-mode)
+		    :help "Clean temporary ConTeXt files")
+	      (list "BibTeX" "bibtex %s" 'TeX-run-BibTeX nil t
+		    :help "Run BibTeX")
+	      (list "View" "%V" 'TeX-run-discard t t :help "Run viewer")
+	      (list "Print" "gsview32 %f" 'TeX-run-command t t
+		    :help "Print the file")
+	      (list "File" "dvips %d -o %f " 'TeX-run-command t t
+		    :help "Generate PostScript file")
+	      (list "Index" "makeindex %s" 'TeX-run-command nil t
+		    :help "Create index file")
+	      (list "Check" "lacheck %s" 'TeX-run-compile nil
+		    (list 'latex-mode)
+		    :help "Check LaTeX file for correctness")
+	      (list "Other" "" 'TeX-run-command t t
+		    :help "Run an arbitrary command"))))
 
 (unless (get 'TeX-view-style 'saved-value)
   (setq TeX-view-style '(("^epsf$" "gsview32 %f")
@@ -50,9 +72,9 @@
 
 (unless (get 'TeX-output-view-style 'saved-value)
   (setq TeX-output-view-style
-	'(("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "dvips %d -o && gsview %f")
+	'(("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "dvips %d -o && gsview32 %f")
 	  ("^dvi$" "." "yap -1 %dS %d")
-	  ("^pdf$" "." "AcroRd32 %o")
+	  ("^pdf$" "." "AcroRd32 %o") ; Use "start %o" instead?
 	  ("^html?$" "." "mozilla %o"))))
 
 (unless (get 'TeX-source-specials-viewer-flags 'saved-value)
