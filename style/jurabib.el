2bf5653c3fbc8d15e4b105201446a5c05e5896c0
Ralf Angeli <angeli@caeruleus.net>
2004-12-01 00:20:52 +0000

    *** empty log message ***

--- style/jurabib.el
+++ style/jurabib.el
@@ -49,7 +49,7 @@
 	    ("footcitetitle" . 2) ("footfullcite" . 2)
 	    ("footcitet" . 1) ("footcitealt" . 1)
 	    ("footcitep" . 2) ("footcitealp" . 2)
-	    ("footciteauthor" . 0) ("footciteauthor" . 2))))
+	    ("footciteauthor" . 2) ("footciteyear" . 2))))
      ;; Add these symbols
      (apply 
       'TeX-add-symbols
@@ -96,9 +96,11 @@
 
      ;; Add further symbols
      (TeX-add-symbols
+      '("citefullfirstfortype" 1)
       '("citenotitlefortype" 1)
       '("citeswithoutentry" 1)
       '("citetitlefortype" 1)
+      '("citeworkwithtitle" 1)
       '("nextcitefull" 1)
       '("nextcitenotitle" 1)
       '("nextcitereset" 1)
@@ -151,6 +153,469 @@
 	    (?y    . "\\citeyear{%l}")
 	    (?f    . "\\footcite{%l}")
 	    (?F    . "\\footcite[?][]{%l}")
-	    (?l    . "\\footfullcite{%l}"))))))))
+	    (?l    . "\\footfullcite{%l}"))))))
+
+   ;; FIXME: The following list is the edited output of
+   ;; `TeX-auto-generate' which probably includes internal macros of
+   ;; jurabib.  Unfortunately the macros which should be accessible to
+   ;; the user are not fully documented at the time of this writing.
+   ;; But instead of including only the limited part which is
+   ;; documented we rather give the user a bit too much.  The list
+   ;; should be reduced when there is proper documentation, though.
+   (TeX-add-symbols
+    '("Wrapquotes" 1)
+    '("apyformat" 1)
+    '("artnumberformat" 1)
+    '("artvolnumformat" 2)
+    '("artvolumeformat" 1)
+    '("artyearformat" 1)
+    '("bibAnnote" 1)
+    '("bibAnnoteFile" 1)
+    '("bibAnnotePath" 1)
+    '("bibEIMfont" 1)
+    '("bibIMfont" 1)
+    '("bibYear" 1)
+    '("bibedformat" 1)
+    '("bibedinformat" 1)
+    '("bibenf" 5)
+    '("biblenf" 5)
+    '("bibnf" 5)
+    '("bibnumberformat" 1)
+    '("bibrenf" 5)
+    '("bibrlenf" 5)
+    '("bibrnf" 5)
+    '("biburlfont" 1)
+    '("edfont" 1)
+    '("formatarticlepages" ["argument"] 2)
+    '("fsted" 1)
+    '("fullnameoxfordcrossref" 1)
+    '("incolledformat" 5)
+    '("jbArchPages" 1)
+    '("jbPages" 1)
+    '("jbannoteformat" 1)
+    '("jbapifont" 1)
+    '("jbarchnameformat" 1)
+    '("jbarchsig" 2)
+    '("jbartPages" 1)
+    '("jbartcrossrefchecked" ["argument"] 1)
+    '("jbauthorindexfont" 1)
+    '("jbbibargs" 5)
+    '("jbbibyearformat" 1)
+    '("jbcitationoyearformat" 1)
+    '("jbcitationyearformat" 1)
+    '("jbcrossrefchecked" ["argument"] 1)
+    '("jbedafti" 1)
+    '("jbedbyincollcrossreflong" 1)
+    '("jbedbyincollcrossrefshort" 1)
+    '("jbedbyincollcrossrefshortnoapy" 1)
+    '("jbedbyincollcrossrefshortwithapy" 1)
+    '("jbedition" 1)
+    '("jbeditorindexfont" 1)
+    '("jbendnote" 1)
+    '("jbflanguage" 1)
+    '("jbincollcrossref" 2)
+    '("jbisbn" 1)
+    '("jbissn" 1)
+    '("jbnote" 2)
+    '("jborganizationindexfont" 1)
+    '("jbpagesformat" 1)
+    '("jbprformat" 1)
+    '("jbrealcitation" 2)
+    '("jbshortarchformat" 1)
+    '("jbshortsubarchformat" 1)
+    '("jbsy" 1)
+    '("jbtiafed" 1)
+    '("lookatfortype" 1)
+    '("nobibliography" 1)
+    '("nocitebuthowcited" 1)
+    '("numberandseries" 2)
+    '("pageadd" 1)
+    '("pernumberformat" 1)
+    '("pervolnumformat" 2)
+    '("pervolumeformat" 1)
+    '("peryearformat" 1)
+    '("revnumberformat" 1)
+    '("revvolnumformat" 2)
+    '("revvolumeformat" 1)
+    '("revyearformat" 1)
+    '("snded" 1)
+    '("textitswitch" 1)
+    '("translator" 3)
+    '("volumeformat" 1)
+    "Bibbfsasep"
+    "Bibbfsesep"
+    "Bibbstasep"
+    "Bibbstesep"
+    "Bibbtasep"
+    "Bibbtesep"
+    "Bibchaptername"
+    "Bibetal"
+    "Edbyname"
+    "IbidemMidName"
+    "IbidemName"
+    "NAT"
+    "OpCit"
+    "Reprint"
+    "SSS"
+    "Transfrom"
+    "Volumename"
+    "addtoalllanguages"
+    "afterfoundersep"
+    "aftervolsep"
+    "ajtsep"
+    "alsothesisname"
+    "aprname"
+    "augname"
+    "bibBTsep"
+    "bibJTsep"
+    "bibPageName"
+    "bibPagesName"
+    "bibaesep"
+    "bibaldelim"
+    "bibaltformatalign"
+    "bibandname"
+    "bibanfont"
+    "bibansep"
+    "bibapifont"
+    "bibapyldelim"
+    "bibapyrdelim"
+    "bibarchpagename"
+    "bibarchpagesname"
+    "bibardelim"
+    "bibartperiodhowcited"
+    "bibatsep"
+    "bibauthormultiple"
+    "bibbdsep"
+    "bibbfsasep"
+    "bibbfsesep"
+    "bibbstasep"
+    "bibbstesep"
+    "bibbtasep"
+    "bibbtesep"
+    "bibbtfont"
+    "bibbtsep"
+    "bibbudcsep"
+    "bibces"
+    "bibchapterlongname"
+    "bibchaptername"
+    "bibcite"
+    "bibcolumnsep"
+    "bibcommenthowcited"
+    "bibcontinuedname"
+    "bibcrossrefcite"
+    "bibcrossrefciteagain"
+    "bibeandname"
+    "bibedformat"
+    "bibefnfont"
+    "bibeimfont"
+    "bibelnfont"
+    "bibenf"
+    "bibfnfmt"
+    "bibfnfont"
+    "bibhowcited"
+    "bibibidfont"
+    "bibidemPfname"
+    "bibidemPmname"
+    "bibidemPnname"
+    "bibidemSfname"
+    "bibidemSmname"
+    "bibidemSnname"
+    "bibidempfname"
+    "bibidempmname"
+    "bibidempnname"
+    "bibidemsfname"
+    "bibidemsmname"
+    "bibidemsnname"
+    "bibimfont"
+    "bibincollcrossrefcite"
+    "bibincollcrossrefciteagain"
+    "bibjtfont"
+    "bibjtsep"
+    "bibleftcolumn"
+    "bibleftcolumnadjust"
+    "bibleftcolumnstretch"
+    "biblenf"
+    "biblnfmt"
+    "biblnfont"
+    "bibnf"
+    "bibnotcited"
+    "bibpagename"
+    "bibpagesname"
+    "bibpagesnamesep"
+    "bibpldelim"
+    "bibprdelim"
+    "bibrevtfont"
+    "bibrightcolumn"
+    "bibrightcolumnadjust"
+    "bibrightcolumnstretch"
+    "bibsall"
+    "bibsdanish"
+    "bibsdutch"
+    "bibsenglish"
+    "bibsfinnish"
+    "bibsfrench"
+    "bibsgerman"
+    "bibsitalian"
+    "bibsnfont"
+    "bibsnorsk"
+    "bibsportuguese"
+    "bibsspanish"
+    "bibtabularitemsep"
+    "bibtfont"
+    "bibtotalpagesname"
+    "biburlprefix"
+    "biburlsuffix"
+    "bibvolumecomment"
+    "bibvtfont"
+    "bothaesep"
+    "bpubaddr"
+    "byname"
+    "citetitleonly"
+    "citeyearpar"
+    "commaename"
+    "commaname"
+    "dateldelim"
+    "daterdelim"
+    "decname"
+    "diffpageibidemmidname"
+    "diffpageibidemname"
+    "edbyname"
+    "edbysep"
+    "editionname"
+    "editorname"
+    "editorsname"
+    "enoteformat"
+    "etalname"
+    "etalnamenodot"
+    "febname"
+    "fifthedname"
+    "firstedname"
+    "footcitetitleonly"
+    "formatpages"
+    "foundername"
+    "fourthedname"
+    "fromdutch"
+    "fromenglish"
+    "fromfinnish"
+    "fromfrench"
+    "fromgerman"
+    "fromitalian"
+    "fromnorsk"
+    "fromportuguese"
+    "fromspanish"
+    "herename"
+    "howcitedprefix"
+    "howcitedsuffix"
+    "ibidem"
+    "ibidemmidname"
+    "ibidemname"
+    "idemPfedbyname"
+    "idemPfname"
+    "idemPmedbyname"
+    "idemPmname"
+    "idemPnedbyname"
+    "idemPnname"
+    "idemSfedbyname"
+    "idemSfname"
+    "idemSmedbyname"
+    "idemSmname"
+    "idemSnedbyname"
+    "idemSnname"
+    "idemmidname"
+    "idemname"
+    "idempfedbyname"
+    "idempfname"
+    "idempmedbyname"
+    "idempmname"
+    "idempnedbyname"
+    "idempnname"
+    "idemsfedbyname"
+    "idemsfname"
+    "idemsmedbyname"
+    "idemsmname"
+    "idemsnedbyname"
+    "idemsnname"
+    "incollinname"
+    "inname"
+    "inseriesname"
+    "janname"
+    "jbCheckedFirst"
+    "jbFirst"
+    "jbFirstAbbrv"
+    "jbJunior"
+    "jbLast"
+    "jbNotRevedNoVonJr"
+    "jbNotRevedNoVonNoJr"
+    "jbNotRevedOnlyLast"
+    "jbNotRevedVonJr"
+    "jbNotRevedVonNoJr"
+    "jbPAGES"
+    "jbPageName"
+    "jbPages"
+    "jbPagesName"
+    "jbRevedFirstNoVonJr"
+    "jbRevedFirstNoVonNoJr"
+    "jbRevedFirstOnlyLast"
+    "jbRevedFirstVonJr"
+    "jbRevedFirstVonNoJr"
+    "jbRevedNotFirstNoVonJr"
+    "jbRevedNotFirstNoVonNoJr"
+    "jbRevedNotFirstOnlyLast"
+    "jbRevedNotFirstVonJr"
+    "jbRevedNotFirstVonNoJr"
+    "jbVon"
+    "jbactualauthorfnfont"
+    "jbactualauthorfont"
+    "jbaddtomakehowcited"
+    "jbaensep"
+    "jbafterstartpagesep"
+    "jbannotatorfont"
+    "jbapifont"
+    "jbarchnamesep"
+    "jbarchpagename"
+    "jbarchpagesname"
+    "jbartPages"
+    "jbatsep"
+    "jbauthorfnfont"
+    "jbauthorfont"
+    "jbauthorfontifannotator"
+    "jbauthorinfo"
+    "jbbeforestartpagesep"
+    "jbbfsasep"
+    "jbbfsesep"
+    "jbbookedaftertitle"
+    "jbbstasep"
+    "jbbstesep"
+    "jbbtasep"
+    "jbbtesep"
+    "jbbtfont"
+    "jbbtitlefont"
+    "jbcitationyearformat"
+    "jbcrossrefchecked"
+    "jbdisablecitationcrossref"
+    "jbdoitem"
+    "jbdonotindexauthors"
+    "jbdonotindexeditors"
+    "jbdonotindexorganizations"
+    "jbdotafterbibentry"
+    "jbdotafterendnote"
+    "jbdy"
+    "jbedbyincollcrossrefcite"
+    "jbedbyincollcrossrefciteagain"
+    "jbedition"
+    "jbedseplikecite"
+    "jbeimfont"
+    "jbfirstcitepageranges"
+    "jbfootnoteformat"
+    "jbfootnotenumalign"
+    "jbfulltitlefont"
+    "jbhowcitedcomparepart"
+    "jbhowcitednormalpart"
+    "jbhowsepannotatorfirst"
+    "jbhowsepannotatorlast"
+    "jbhowsepbeforetitle"
+    "jbhowsepbeforetitleae"
+    "jbhowsepbeforetitleibidemname"
+    "jbignorevarioref"
+    "jbimfont"
+    "jbindexbib"
+    "jbindexonlyfirstauthors"
+    "jbindexonlyfirsteditors"
+    "jbindexonlyfirstorganizations"
+    "jbindextype"
+    "jblookforgender"
+    "jbmakeinbib"
+    "jbmakeinbiblist"
+    "jbmakeindexactual"
+    "jbnotsamearch"
+    "jbonlyforbib"
+    "jbonlyforcitations"
+    "jbonlyforfirstcitefullbegin"
+    "jbonlyforfirstcitefullend"
+    "jborgauthorfont"
+    "jboyearincitation"
+    "jbpagename"
+    "jbpagenamenodot"
+    "jbpages"
+    "jbpagesep"
+    "jbpagesname"
+    "jbpagesnamesep"
+    "jbsamearch"
+    "jbsamesubarch"
+    "jbsamesubarchindent"
+    "jbshorttitlefont"
+    "jbshowbibextralabel"
+    "jbssedbd"
+    "jbsubarchsep"
+    "jbsuperscripteditionafterauthor"
+    "jbtitlefont"
+    "jbts"
+    "jburldef"
+    "jbuseidemhrule"
+    "jbyear"
+    "jbyearaftertitle"
+    "julname"
+    "junname"
+    "jurthesisname"
+    "marname"
+    "mastersthesisname"
+    "mayname"
+    "nofirstnameforcitation"
+    "noibidem"
+    "noidem"
+    "nopage"
+    "novname"
+    "numbername"
+    "octname"
+    "ofseriesname"
+    "opcit"
+    "organizationname"
+    "origPAGES"
+    "origartPages"
+    "origbibces"
+    "origcrossref"
+    "origpages"
+    "osep"
+    "phdthesisname"
+    "reprint"
+    "reprintname"
+    "reviewbyname"
+    "reviewname"
+    "reviewofname"
+    "samepageibidemmidname"
+    "samepageibidemname"
+    "secondedname"
+    "sepname"
+    "sndecmd"
+    "snded"
+    "sndeditorname"
+    "sndeditorsname"
+    "technicalreportname"
+    "testnosig"
+    "textandname"
+    "texteandname"
+    "theHlvla"
+    "theHlvlb"
+    "theHlvlc"
+    "theHlvld"
+    "theHlvle"
+    "theHlvlf"
+    "theHlvlg"
+    "theHlvlh"
+    "theHlvli"
+    "theHlvlj"
+    "theHlvlk"
+    "theHlvll"
+    "thedname"
+    "thirdedname"
+    "trans"
+    "transby"
+    "transfrom"
+    "updatename"
+    "updatesep"
+    "urldatecomment"
+    "volname"
+    "volumename"
+    "volumeofname")))
 
 ;;; jurabib.el ends here
