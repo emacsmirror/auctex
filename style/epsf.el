71e108b406d3b6633d4caf777c927ca0d2dd7f74
Ralf Angeli <angeli@caeruleus.net>
2005-02-11 08:51:40 +0000

    ("epsf"): Do not add epsffig environment.  Fixes error if latex.el(c) is not loaded. (LaTeX-env-epsffigure): Remove.

--- style/epsf.el
+++ style/epsf.el
@@ -4,73 +4,17 @@
 
 ;;; Code: 
 
-(TeX-add-style-hook "epsf"
- (function
-  (lambda ()
-    (TeX-add-symbols
-     '("epsfsize" TeX-arg-epsfsize)
-     '("epsffile" TeX-arg-file)
-     '("epsfbox" TeX-arg-file)
-     "epsflly" "epsfury" "testit" "epsfgetlitbb"
-     "epsfnormal" "epsfgetbb" "other" "epsfsetgraph"
-     "PsFragSpecialArgs" "epsfaux" "testit" "epsfgrab"
-     "epsfllx" "epsflly" "epsfury" "epsfverbosetrue"
-     )
-    (LaTeX-add-environments
-     '("epsffig" LaTeX-env-epsffigure)
-     )
-
-    )))
-
-
-(defun LaTeX-env-epsffigure (environment)
-  "Create a `figure'-environment with \\label and \\caption and \\epsfbox
-commands. Eventually a `psfrags'-environment is inserted round the \\epsfbox."
-
-  (let ((float (read-input "Float to: " LaTeX-float))
-	(caption (read-input "Caption: "))
-	(label (read-input "Label: " LaTeX-figure-label))
-        ; gf: ask if there is an psfrag environment needed
-	(psfrag (y-or-n-p "PS fragments: "))
-	(psfile (read-file-name "EPS-file: " "" "" nil))
-	)
-
-    (setq LaTeX-float (if (zerop (length float))
-			  LaTeX-float
-			float))
-
-    (LaTeX-insert-environment "figure"
-			      (concat LaTeX-optop LaTeX-float LaTeX-optcl))
-    (LaTeX-insert-environment "center")
-    (if psfrag
-	(progn
-	  (LaTeX-insert-environment "psfrags")
-	  (newline-and-indent)
-	  ))
-    (if (or (zerop (length label))
-	    (and (string= "figure" environment)
-		 (equal LaTeX-figure-label label))
-	    )
-	()
-      (newline-and-indent)
-      (insert TeX-esc "label" TeX-grop label TeX-grcl)
-      (end-of-line 0)
-      (LaTeX-indent-line))
-
-
-    (newline-and-indent)
-    (insert TeX-esc "leavevmode")
-    (newline-and-indent)
-    (insert TeX-esc "epsfbox" TeX-grop psfile TeX-grcl)
-    (if (zerop (length caption))
-	()
-      (newline-and-indent)
-      (insert TeX-esc "caption" TeX-grop caption TeX-grcl))
-    (newline)
-    (forward-line 4)
-    (newline)
-
-))
+(TeX-add-style-hook
+ "epsf"
+ (lambda ()
+   (TeX-add-symbols
+    '("epsfsize" TeX-arg-epsfsize)
+    '("epsffile" TeX-arg-file)
+    '("epsfbox" TeX-arg-file)
+    "epsflly" "epsfury" "testit" "epsfgetlitbb"
+    "epsfnormal" "epsfgetbb" "other" "epsfsetgraph"
+    "PsFragSpecialArgs" "epsfaux" "testit" "epsfgrab"
+    "epsfllx" "epsflly" "epsfury" "epsfverbosetrue")))
 
 (defun TeX-arg-epsfsize (optional &optional prompt definition)
   "Create a line that print epsf figures at a certain percentage"
