2b0fc1f0163f9e1c1602d64533b5f94199569eee
Ralf Angeli <angeli@caeruleus.net>
2005-02-13 14:24:13 +0000

    (Texinfo-environment, Texinfo-modify-environment): New functions. (Texinfo-insert-environment): Not interactive anymore as it is now called by `Texinfo-environment'. (Texinfo-mode-map): `C-c C-e' now calls `Texinfo-environment'.

--- tex-info.el
+++ tex-info.el
@@ -1,7 +1,7 @@
 ;;; tex-info.el --- Support for editing Texinfo source.
 
 ;; Copyright (C) 1993, 1994, 1997, 2000, 2001,
-;;               2004 Free Software Foundation, Inc.
+;;               2004, 2005 Free Software Foundation, Inc.
 
 ;; Maintainer: auc-tex@sunsite.dk
 ;; Keywords: tex
@@ -56,11 +56,20 @@
   "Regexp for environment-like Texinfo list commands.
 Subexpression 1 is what goes into the corresponding `@end' statement.")
 
-(defun Texinfo-insert-environment (env)
-  "Insert Texinfo environment ENV.
-When called interactively, prompt for an environment."
+(defun Texinfo-environment (env &optional arg)
+  "Make Texinfo environment ENV.
+With optional ARG, modify current environment."
+  ;; XXX: This could be enhanced to act like `LaTeX-environment',
+  ;; i.e. suggest a default environment and have its own history.
   (interactive (list (completing-read "Environment: "
-				      Texinfo-environment-list)))
+				      Texinfo-environment-list)
+		     current-prefix-arg))
+  (if arg
+      (Texinfo-modify-environment env)
+    (Texinfo-insert-environment env)))
+
+(defun Texinfo-insert-environment (env)
+  "Insert Texinfo environment ENV."
   (if (and (TeX-active-mark)
 	   (not (eq (mark) (point))))
       (progn
@@ -79,6 +88,19 @@ When called interactively, prompt for an environment."
     (insert "@" env "\n\n@end " env "\n")
     (if (null (cdr-safe (assoc "defcv" Texinfo-environment-list)))
 	(forward-line -2))))
+
+(defun Texinfo-modify-environment (env)
+  "Change current environment to environment ENV."
+  (save-excursion
+    (Texinfo-find-env-end)
+    (re-search-backward (concat (regexp-quote TeX-esc) "end \\([a-zA-Z]*\\)")
+			(line-beginning-position))
+    (replace-match env t t nil 1)
+    (beginning-of-line)
+    (Texinfo-find-env-start)
+    (re-search-forward (concat (regexp-quote TeX-esc) "\\([a-zA-Z]*\\)")
+		       (line-end-position))
+    (replace-match env t t nil 1)))
 
 (defun Texinfo-find-env-end ()
   "Move point to the end of the current environment."
@@ -196,7 +218,7 @@ for @node."
     (define-key map "\C-c\C-u\C-a"   'texinfo-all-menus-update)
 
     ;; Simulating LaTeX-mode
-    (define-key map "\C-c\C-e" 'Texinfo-insert-environment)
+    (define-key map "\C-c\C-e" 'Texinfo-environment)
     (define-key map "\C-c\n"   'texinfo-insert-@item)
     (or (key-binding "\e\r")
 	(define-key map "\e\r" 'texinfo-insert-@item)) ;*** Alias
