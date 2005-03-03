facb9e7bf638fb943f7668a608ba7406ca66658c
David Kastrup <dak@gnu.org>
2005-03-03 17:50:48 +0000

    (preview-make-package-xemacs): Add directory information from autoe.el to autoloads.

--- preview/prv-install.el
+++ preview/prv-install.el
@@ -102,7 +102,7 @@ number of Lisp files to generate autoloads from."
     (when (file-exists-p generated-autoload-file)
       (with-temp-buffer (insert-file "auto.el")
 			(append-to-file (point-min) (point-max)
-					(generated-autoload-file)))
+					generated-autoload-file))
       (byte-compile-file generated-autoload-file)
       (push "auto-autoloads.el" els))
     ; Some people delete the pkginfo directory; this should not break
