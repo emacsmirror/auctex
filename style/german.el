564fc886e33360d6029208502fe4807793d649dc
Ralf Angeli <angeli@caeruleus.net>
2005-03-28 21:49:38 +0000

    (LaTeX-mode-map): Bind `-' to `LaTeX-german-insert-hyphen'. (LaTeX-german-quote-after-quote, LaTeX-german-open-quote) (LaTeX-german-close-quote): Move to tex-style.el. (LaTeX-german-hyphen-internal): New variable. (LaTeX-german-insert-hyphen): New function. ("german"): Set `LaTeX-german-hyphen-internal'.

--- style/german.el
+++ style/german.el
@@ -1,10 +1,10 @@
-;;; german.el - Setup AUC TeX for editing German text.
-
-;; $Id: german.el,v 1.8 2004-08-22 16:45:21 dak Exp $
+;;; german.el --- Setup AUCTeX for editing German text.
 
 ;;; Commentary:
 ;;
-;; `german.sty' use `"' to give next character an umlaut.
+;; Cater for some specialities of `(n)german.sty', e.g. special quote
+;; and hyphen strings or that `"' makes the following letter an
+;; umlaut.
 
 ;;; Code:
 
@@ -13,26 +13,61 @@
   "Syntax table used in LaTeX mode when using `german.sty'.")
 
 (modify-syntax-entry ?\"  "w"  LaTeX-german-mode-syntax-table)
+(define-key LaTeX-mode-map "-" 'LaTeX-german-insert-hyphen)
 
-(defvar LaTeX-german-quote-after-quote t
-  "Initial value of `TeX-quote-after-quote' for `german.el'")
-(defvar LaTeX-german-open-quote "\"`"
-  "Initial value of `TeX-open-quote' for `german.el'")
-(defvar LaTeX-german-close-quote "\"'"
-  "Initial value of `TeX-close-quote' for `german.el'")
+(defvar LaTeX-german-hyphen-internal nil)
+(make-variable-buffer-local 'LaTeX-german-hyphen-internal)
+(defun LaTeX-german-insert-hyphen (force)
+  "Insert a hyphen string.
+The string can be either a normal hyphen or the string specified
+in `LaTeX-german-hyphen'.  Wether one or the other is chosen
+depends on the value of `LaTeX-german-hyphen-after-hyphen' and
+the buffer context.
+If prefix argument FORCE is non-nil, always insert a regular hyphen."
+  (interactive "*P")
+  (if (or force (zerop (length LaTeX-german-hyphen-internal)))
+      (call-interactively 'self-insert-command)
+    ;; FIXME: It would be nice to check for verbatim constructs in the
+    ;; non-font-locking case, but things like `LaTeX-current-environment'
+    ;; are rather expensive in large buffers.
+    (if (or (and (fboundp 'font-latex-faces-present-p)
+		 (font-latex-faces-present-p '(font-latex-verbatim-face
+					       font-latex-math-face
+					       font-lock-comment-face)))
+	    (texmathp)
+	    (TeX-in-comment))
+	(call-interactively 'self-insert-command)
+      (let ((hyphen-length (length LaTeX-german-hyphen)))
+	(cond
+	 ((and (> hyphen-length 1)
+	       (not LaTeX-german-hyphen-after-hyphen)
+	       (string= (buffer-substring (- (point) hyphen-length) (point))
+			LaTeX-german-hyphen))
+	  (delete-backward-char hyphen-length)
+	  (call-interactively 'self-insert-command))
+	 ((and (> hyphen-length 1)
+	       LaTeX-german-hyphen-after-hyphen
+	       (eq (char-before) ?-))
+	  (delete-backward-char 1)
+	  (insert LaTeX-german-hyphen))
+	 (LaTeX-german-hyphen-after-hyphen
+	  (call-interactively 'self-insert-command))
+	 (t (insert LaTeX-german-hyphen)))))))
 
-(TeX-add-style-hook "german"
- (function (lambda ()
-	     (set-syntax-table LaTeX-german-mode-syntax-table)
-	     (unless (local-variable-p 'TeX-open-quote (current-buffer))
-	       (make-local-variable 'TeX-open-quote)
-	       (setq TeX-open-quote LaTeX-german-open-quote))
-	     (unless (local-variable-p 'TeX-close-quote (current-buffer))
-	       (make-local-variable 'TeX-close-quote)
-	       (setq TeX-close-quote LaTeX-german-close-quote))
-	     (unless (local-variable-p 'TeX-quote-after-quote (current-buffer))
-	       (make-local-variable 'TeX-quote-after-quote)
-	       (setq TeX-quote-after-quote LaTeX-german-quote-after-quote))
-	     (run-hooks 'TeX-language-de-hook))))
+(TeX-add-style-hook
+ "german"
+ (lambda ()
+   (set-syntax-table LaTeX-german-mode-syntax-table)
+   (unless (local-variable-p 'TeX-open-quote (current-buffer))
+     (make-local-variable 'TeX-open-quote)
+     (setq TeX-open-quote LaTeX-german-open-quote))
+   (unless (local-variable-p 'TeX-close-quote (current-buffer))
+     (make-local-variable 'TeX-close-quote)
+     (setq TeX-close-quote LaTeX-german-close-quote))
+   (unless (local-variable-p 'TeX-quote-after-quote (current-buffer))
+     (make-local-variable 'TeX-quote-after-quote)
+     (setq TeX-quote-after-quote LaTeX-german-quote-after-quote))
+   (run-hooks 'TeX-language-de-hook)
+   (setq LaTeX-german-hyphen-internal LaTeX-german-hyphen)))
 
 ;;; german.el ends here
