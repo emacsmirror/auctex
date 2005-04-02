57d3b14573d675976baf3357360563521bf6f1dc
David Kastrup <dak@gnu.org>
2005-04-02 20:28:57 +0000

    * doc/macros.texi: Synchronize to AUCTeX.
    * Makefile.in (tarball-ready): Remove this target for now: it
    can't be used consistently really.  It is probably better to do
    just run autogen.sh on a freshly exported archive.
    
    * doc/Makefile.in (DISTTEXTS): Targets in parent dir.
    (../INSTALL, ../INSTALL.windows, ../README, ../FAQ, ../PROBLEMS)
    (../TODO): New targets instead of targets in current dir.
    (clean): Don't remove targets in this dir.
    (maintainer-clean): Remove DISTTEXTS targets.
    
    * autogen.sh: Use make for generating distribution texts and also
    pregenerate the info file.
    
    * Makefile.in (emacsprefix): Remove.
    (.PHONY): Collect phony targets here.
    (DISTTEXTS): Move to doc/Makefile.in
    (tarball-ready): Adapt

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -3,12 +3,6 @@ if test -z "$AUTOCONF"
 then autoconf
 else $AUTOCONF
 fi
-
 cd doc
-makeinfo -D rawfile --no-headers --no-validate readme.texi --output ../README
-makeinfo -D rawfile --no-headers --no-validate install.texi --output ../INSTALL
-makeinfo -D rawfile --no-headers --no-validate problems.texi --output ../PROBLEMS
-makeinfo -D rawfile --no-headers --no-validate todo.texi --output ../TODO
-makeinfo -D rawfile --no-headers --no-validate wininstall.texi --output ../INSTALL.windows
-makeinfo -D rawfile --no-headers --no-validate --number-sections faq.texi --output ../FAQ
+make -f Makefile.in MAKEINFO=makeinfo disttexts preview-latex.info
 cd ..
