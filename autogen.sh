2d329ba47d97f6f5f3baf1f029fe4d090220e808
Ralf Angeli <angeli@caeruleus.net>
2005-06-08 07:53:21 +0000

    Generate preview-latex docs as well.

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 test "x${AUTOCONF}" = x && AUTOCONF=autoconf
 test "x${MAKEINFO}" = x && MAKEINFO=makeinfo
+test "x${PERL}" = x && PERL=perl
 ${AUTOCONF}
 rm -rf autom4te.cache
 cd doc
-make -f Makefile.in MAKEINFO="${MAKEINFO}" disttexts auctex.info
+make -f Makefile.in MAKEINFO="${MAKEINFO}" PERL="$PERL" disttexts auctex.info preview-latex.info
 cd ..
 cd preview
 export AUTOCONF
