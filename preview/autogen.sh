8f142946179f121d1bb653648cbca23d22564705
Ralf Angeli <angeli@caeruleus.net>
2005-06-08 07:32:24 +0000

    Do not generate documentation anymore.

--- preview/autogen.sh
+++ preview/autogen.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 test "x${AUTOCONF}" = x && AUTOCONF=autoconf
-test "x${MAKEINFO}" = x && MAKEINFO=makeinfo
-test "x${PERL}" = x && PERL=perl
 ${AUTOCONF} -I ..
 rm -rf autom4te.cache
-cd doc
-make -f Makefile.in MAKEINFO="$MAKEINFO" PERL="$PERL" disttexts preview-latex.info
-cd ..
