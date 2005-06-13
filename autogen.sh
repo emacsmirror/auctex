cedde085baacb01448f6a9456c1e194c0cdc792c
David Kastrup <dak@gnu.org>
2005-06-13 23:20:15 +0000

    * doc/Makefile.in (all): Same as dist. (dist): Just create DISTTEXTS and info files. (extradist): Additional stuff for ftp site. (disttexts): Remove target. (clean): Don't remove info files. (maintainer-clean): Remove info files.
    * autogen.sh: Add error messages, run autoconf in preview
    subdirectory.  Heed MAKE variable if set.
    
    * Makefile.in (CVSFILES): Define files not to use in tarball.
    (tar-ball): Remove them.
    (tar-ball): Make extradist for additional doc files on ftp.
    
    * README.CVS: Remove.  Top file should be sufficient.
    
    * autogen.sh: Remove.  We have its functionality in the top
    directory.

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-test "x${AUTOCONF}" = x && AUTOCONF=autoconf
-test "x${MAKEINFO}" = x && MAKEINFO=makeinfo
-test "x${PERL}" = x && PERL=perl
-${AUTOCONF}
-rm -rf autom4te.cache
-cd doc
-make -f Makefile.in MAKEINFO="${MAKEINFO}" PERL="$PERL" disttexts auctex.info preview-latex.info
-cd ..
+test "x${AUTOCONF}" != x || AUTOCONF=autoconf
+test "x${MAKEINFO}" != x || MAKEINFO=makeinfo
+test "x${PERL}" != x || PERL=perl
+test "x${MAKE}" != x || MAKE=make
+${AUTOCONF} || { echo "Error running ${AUTOCONF} in ." >&2 ; exit 1; }
 cd preview
-export AUTOCONF
-export MAKEINFO
-./autogen.sh
+${AUTOCONF} -I.. || { echo "Error running ${AUTOCONF} in preview" >&2 ; exit 1; }
 cd ..
+rm -rf autom4te.cache preview/autom4te.cache
+cd doc
+${MAKE} -f Makefile.in MAKEINFO="${MAKEINFO}" PERL="$PERL" dist distclean || { echo "Error running ${MAKE} in doc" >&2 ; exit 1; }
+cd ..
+
 
--- preview/autogen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-test "x${AUTOCONF}" = x && AUTOCONF=autoconf
-${AUTOCONF} -I ..
-rm -rf autom4te.cache
