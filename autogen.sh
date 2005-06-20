e47e7012991647b793063536068145ad880fd9e1
David Kastrup <dak@gnu.org>
2005-06-20 21:58:53 +0000

    * doc/install.texi (Advice for package providers): Propose package structure.
    * doc/Makefile.in (install-man): Install sources for texinfo
    files.
    (.PHONY): Add install-man.
    
    * Makefile.in (.PHONY): Add install-man and xemacs-package
    (install-man): Just descend into doc directory.
    (xemacs-package): Try building some XEmacs package.
    
    * autogen.sh: Don't run distclean, that seems unexpected.

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ ${AUTOCONF} -I.. || { echo "Error running ${AUTOCONF} in preview" >&2 ; exit 1; 
 cd ..
 rm -rf autom4te.cache preview/autom4te.cache
 cd doc
-${MAKE} -f Makefile.in MAKEINFO="${MAKEINFO}" PERL="$PERL" dist distclean || { echo "Error running ${MAKE} in doc" >&2 ; exit 1; }
+${MAKE} -f Makefile.in MAKEINFO="${MAKEINFO}" PERL="$PERL" dist || { echo "Error running ${MAKE} in doc" >&2 ; exit 1; }
 cd ..
 
 
