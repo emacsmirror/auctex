ddfb1396ca89e368fccf09889676d11bcb67b434
David Kastrup <dak@gnu.org>
2006-09-29 09:13:26 +0000

    Fix syntax errors.

--- autogen.sh
+++ autogen.sh
@@ -9,11 +9,13 @@ ${AUTOCONF} -I.. || { echo "Error running ${AUTOCONF} in preview" >&2 ; exit 1; 
 cd ..
 rm -rf autom4te.cache preview/autom4te.cache
 if test "x${AUCTEXDATE}" == x
+then
     AUCTEXDATE=`sed -n '1s/^\([-0-9][-0-9]*\).*/\1/p' ChangeLog`
     test "X${AUCTEXDATE}" != X || { echo "Can't find date in ChangeLog" >&2 ; exit 1; }
 fi
 
 if test "x${AUCTEXVERSION}" == x
+then
     AUCTEXVERSION=`sed -n '2,/^[0-9]/s/.*Version \(.*\) released\..*/\1/p' ChangeLog`
     test "X${AUCTEXVERSION}" != X || AUCTEXVERSION=${AUCTEXDATE}
 fi
