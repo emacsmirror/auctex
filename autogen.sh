ffda388ddff93b9ca4799da4483e2cbd4ca80ec7
David Kastrup <dak@gnu.org>
2006-09-29 09:11:37 +0000

    * doc/Makefile.in (version.texi): Let version.texi depend rather on ChangeLog in order not to disturb pregenerated documentation.
    * autogen.sh: Prepare `AUCTEXDATE' and `AUCTEXVERSION' for `make
    dist'.
    
    * Makefile.in (tar-ball): Pass version and date into autogen.sh
    
    * doc/Makefile.in (AUCTEXDATE, AUCTEXVERSION): get from configure.
    (AUCTEXTEXIFILES): Add fdl.texi and version.texi
    (version.texi): create depending on Makefile.
    (maintainer-clean): Remove version.texi.  Should this be in the
    distclean target instead?
    
    * Makefile.in (DOCFILES): Remove, unused.
    (doc/Makefile): Add target.
    (info, dvi, install-man, install-docs, clean, distclean)
    (tar-ball): depend on doc/Makefile

--- autogen.sh
+++ autogen.sh
@@ -8,8 +8,19 @@ cd preview
 ${AUTOCONF} -I.. || { echo "Error running ${AUTOCONF} in preview" >&2 ; exit 1; }
 cd ..
 rm -rf autom4te.cache preview/autom4te.cache
+if test "x${AUCTEXDATE}" == x
+    AUCTEXDATE=`sed -n '1s/^\([-0-9][-0-9]*\).*/\1/p' ChangeLog`
+    test "X${AUCTEXDATE}" != X || { echo "Can't find date in ChangeLog" >&2 ; exit 1; }
+fi
+
+if test "x${AUCTEXVERSION}" == x
+    AUCTEXVERSION=`sed -n '2,/^[0-9]/s/.*Version \(.*\) released\..*/\1/p' ChangeLog`
+    test "X${AUCTEXVERSION}" != X || AUCTEXVERSION=${AUCTEXDATE}
+fi
+
 cd doc
-${MAKE} -f Makefile.in MAKEINFO="${MAKEINFO}" PERL="$PERL" dist || { echo "Error running ${MAKE} in doc" >&2 ; exit 1; }
+rm -f version.texi
+${MAKE} -f Makefile.in MAKEINFO="${MAKEINFO}" PERL="$PERL" AUCTEXDATE="$AUCTEXDATE" AUCTEXVERSION="$AUCTEXVERSION" dist || { echo "Error running ${MAKE} in doc" >&2 ; exit 1; }
 cd ..
 
 
