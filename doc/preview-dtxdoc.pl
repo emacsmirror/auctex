4fb6e2866506861eeff73b782cbe5ee23ea4fa43
Ralf Angeli <angeli@caeruleus.net>
2008-02-03 15:18:38 +0000

    Add coypright and license notices.

--- doc/preview-dtxdoc.pl
+++ doc/preview-dtxdoc.pl
@@ -1,7 +1,33 @@
 #!/usr/bin/perl
+
+# Autoconverter from preview.dtx to preview-dtxdoc.texi
+
+# Author: Jan-Åke Larsson <jalar@mai.liu.se>
+# Maintainer: auctex-devel@gnu.org
+
+# Copyright (C) 2002, 2005 Free Software Foundation, Inc.
+
+# This file is part of AUCTeX.
+
+# AUCTeX is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# AUCTeX is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with AUCTeX; see the file COPYING.  If not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+# Commentary:
+
 # Simpleminded autoconverter from preview.dtx to preview-dtxdoc.texi
 # run as 'perl preview-dtxdoc.pl ../latex/preview.dtx preview-dtxdoc.texi'
-# Author Jan-Åke Larsson <jalar@mai.liu.se>
 
 die "Usage: perl preview-dtxdoc.pl infile outfile" unless ($#ARGV == 1);
 open(STDIN, $ARGV[0]) || die "Can't open $ARGV[0] for reading";
