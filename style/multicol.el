1d52d9242d841b42110f33f2aacf62a6833c91c1
Ralf Angeli <angeli@caeruleus.net>
2011-03-13 20:01:23 +0000

    Add copyright notice and license blurb. Simplify environment definitions.  Provide completion for lengths the package defines.  Get rid of indentation and filling barrier. Remove fontification definitions.

--- style/multicol.el
+++ style/multicol.el
@@ -1,9 +1,28 @@
 ;;; multicol.el --- AUCTeX style for `multicol.sty'
 
-;; Author: Mads Jensen <mje@iwi.uni-sb.de>
+;; Copyright (C) 2011 Free Software Foundation, Inc.
+
+;; Author: Mads Jensen <mje@inducks.org>
 ;; Maintainer: auctex-devel@gnu.org
 ;; Created: 2011-01-24
 ;; Keywords: tex
+
+;; This file is part of AUCTeX.
+
+;; AUCTeX is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; AUCTeX is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with AUCTeX; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
 
 ;;; Commentary:
 
@@ -14,36 +33,30 @@
 (TeX-add-style-hook
  "multicol"
  (lambda ()
-   (LaTeX-add-environments 
-    '("multicols" (lambda (env &rest ignore)
-                    (LaTeX-insert-environment
-                     env (format "{%i}" (read-string "Number of columns: ")))))
-    '("multicols*" (lambda (env &rest ignore)
-                     (LaTeX-insert-environment
-                      env (format "{%i}" (read-string "Number of columns: "))))))
+   (LaTeX-add-environments
+    '("multicols" "Number of columns" [ "Text across columns" ]
+      [ "Local value for \\premulticols" ])
+    '("multicols*" "Number of columns" [ "Text across columns" ]
+      [ "Local value for \\premulticols" ]))
 
-   ;; not sure whether stuff like \columnseprulecolor and \columnseprule
-   ;; should be added, and if so, as what? I couldn't find any sensible
-   ;; snippet from the other style-files which matched the criteria
-   (TeX-add-symbols 
-    '("columnseprule" TeX-arg-macro "Length"))
+   (TeX-add-symbols
+    '("multicoltolerance" 0)
+    '("multicolpretolerance" 0)
+    '("premulticols" 0)
+    '("postmulticols" 0)
+    '("multicolsep" 0)
+    '("multicolbaselineskip" 0)
+    '("multicolovershoot" 0)
+    '("multicolundershoot" 0)
+    '("columnsep" 0)
+    '("columnseprule" 0)
+    '("columnseprulecolor" 0)
+    '("raggedcolumns" 0)
+    '("flushcolumns" 0)
+    "columnbreak")))
 
-   (add-to-list 'LaTeX-indent-environment-list
-		'("multicols*" current-indentation)
-                '("multicols" current-indentation))
-
-   ;; Fontification
-   (when (and (featurep 'font-latex)
-	      (eq TeX-install-font-lock 'font-latex-setup))
-     ;; Tell font-lock about the update.
-     (font-latex-add-keywords '("columnbreak") 'function)
-     (font-latex-add-keywords '("columnseprule") 'function)
-     (font-latex-add-keywords '("columnseprulecolor") 'function)     
-     (setq font-lock-set-defaults nil)
-     (font-lock-set-defaults))))
-
-;; there weren't any mentioned in the manual; I might have missed them
-(defvar LaTeX-multicol-package-options nil
+(defvar LaTeX-multicol-package-options
+  '("errorshow" "infoshow" "balancingshow" "markshow" "debugshow" "grid")
   "Package options for the multicol package.")
 
 ;;; multicol.el ends here
