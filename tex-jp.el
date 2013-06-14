60a707d239a21faf2c0f39be211ba964270d0621
Tassilo Horn <tsdh@gnu.org>
2013-06-14 20:56:44 +0200

    Put coding cookie in elisp header line.

--- tex-jp.el
+++ tex-jp.el
@@ -1,6 +1,4 @@
-;; -*- coding: iso-2022-jp-unix; -*-
-
-;;; tex-jp.el --- Support for Japanese TeX.
+;;; tex-jp.el --- Support for Japanese TeX.  -*- coding: iso-2022-jp-unix; -*-
 
 ;; Copyright (C) 2002-2007, 2012  Free Software Foundation, Inc.
 ;; Copyright (C) 1999, 2001 Hidenobu Nabetani <nabe@debian.or.jp>
