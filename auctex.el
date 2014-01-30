e84716ead776e17171274316a8310f4be6043228
Stefan Monnier <monnier@iro.umontreal.ca>
2014-01-30 08:41:48 -0500

    * auctex.el: Add copyright notice.

--- auctex.el
+++ auctex.el
@@ -1,7 +1,24 @@
 ;;; auctex.el --- Integrated environment for *TeX*
 
+;; Copyright (C) 2014 Free Software Foundation, Inc.
+
 ;; Version: 11.87.2
 ;; URL: http://www.gnu.org/software/auctex/
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
