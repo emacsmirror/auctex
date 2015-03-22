6620d8c11fb0a72a7db1ac7b6a03ab09658fe3e2
Tassilo Horn <tsdh@gnu.org>
2015-03-22 10:25:58 +0100

    Merge from upstream master; release 11.88.3

--- tex-site.el
+++ tex-site.el
@@ -148,11 +148,11 @@ set it with `TeX-modes-set'."
 		       `(TeX-modes-set ',var ,var t))
 		     (setq list (cdr list)))))) )
 
-(defconst AUCTeX-version "11.88"
+(defconst AUCTeX-version "11.88.3"
     "AUCTeX version.
 If not a regular release, the date of the last change.")
 
-(defconst AUCTeX-date "2014-10-29"
+(defconst AUCTeX-date "2015-03-22"
   "AUCTeX release date using the ISO 8601 format, yyyy-mm-dd.")
 
 ;; Store bibitems when saving a BibTeX buffer
