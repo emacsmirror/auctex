d9f61d553ba96bbe0ec194b314d542121a1e60cd
Tassilo Horn <tsdh@gnu.org>
2016-01-31 08:15:34 +0100

    Capture message of LaTeX warnings
    * tex-buf.el (TeX-parse-error): Capture message of LaTeX warnings.

--- tex-buf.el
+++ tex-buf.el
@@ -2270,7 +2270,7 @@ Return non-nil if an error or warning is found."
 	  "^\\(\\(?:Overfull\\|Underfull\\|Tight\\|Loose\\)\
  \\\\.*?[0-9]+--[0-9]+\\)\\|"
 	  ;; LaTeX warning
-	  "^\\(LaTeX [A-Za-z]*\\|Package [A-Za-z0-9]+ \\)Warning:.*"))
+	  "^\\(\\(?:LaTeX [A-Za-z]*\\|Package [A-Za-z0-9]+ \\)Warning:.*\\)"))
 	(error-found nil))
     (while
 	(cond
