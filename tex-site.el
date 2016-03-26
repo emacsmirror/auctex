f1abcf8e27d8de4a7e1f10b3030e0e368380356a
Tassilo Horn <tsdh@gnu.org>
2016-03-26 22:33:59 +0100

    Upgrade docs for release
    * Version 11.89.2 released.

--- tex-site.el
+++ tex-site.el
@@ -148,7 +148,7 @@ set it with `TeX-modes-set'."
 		       `(TeX-modes-set ',var ,var t))
 		     (setq list (cdr list)))))) )
 
-(defconst AUCTeX-version "11.89.1"
+(defconst AUCTeX-version "11.89.2"
     "AUCTeX version.
 If not a regular release, the date of the last change.")
 
