99df5e2a50aac4251a999ba99fef09069f680478
Tassilo Horn <tsdh@gnu.org>
2016-10-14 21:32:57 +0200

    Release GNU AUCTeX 11.89.6
    * Version 11.89.6 released.

--- auctex.el
+++ auctex.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2014-2016 Free Software Foundation, Inc.
 
-;; Version: 11.89.5
+;; Version: 11.89.6
 ;; URL: http://www.gnu.org/software/auctex/
 ;; Maintainer: auctex-devel@gnu.org
 ;; Notifications-To: auctex-diffs@gnu.org
--- tex-site.el
+++ tex-site.el
@@ -150,11 +150,11 @@ set it with `TeX-modes-set'."
 		       `(TeX-modes-set ',var ,var t))
 		     (setq list (cdr list)))))) )
 
-(defconst AUCTeX-version "11.89.5"
-    "AUCTeX version.
+(defconst AUCTeX-version "11.89.6"
+  "AUCTeX version.
 If not a regular release, the date of the last change.")
 
-(defconst AUCTeX-date "2016-09-01"
+(defconst AUCTeX-date "2016-10-14"
   "AUCTeX release date using the ISO 8601 format, yyyy-mm-dd.")
 
 ;; Store bibitems when saving a BibTeX buffer
