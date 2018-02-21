5592c69435c03110e207ef05feed96741cb94d1b
Alex Branham <branham@utexas.edu>
2018-02-21 21:33:35 +0100

    Improve flymake documentation
    * doc/changes.texi:
    * doc/auctex.texi (Checking): Add Flymake support to manual.
    
    * latex-flymake.el: Add copyright notice and fix a quote.
    
    Signed-off-by: Arash Esbati <arash@gnu.org>

--- latex-flymake.el
+++ latex-flymake.el
@@ -1,11 +1,36 @@
 ;;; latex-flymake.el --- Flymake integration  -*- lexical-binding: t; -*-
 
+;; Copyright (C), 2018 Free Software Foundation, Inc.
+
+;; Author: Alex Branham <branham@utexas.edu>
+;; Maintainer: auctex-devel@gnu.org
+;; Created: 2018-02-11
+;; Keywords: tex
+
+;; This file is part of AUCTeX.
+
+;; AUCTeX is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; AUCTeX is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with AUCTeX; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+
 ;;; Commentary:
+
 ;; This file provides flymake integration for latex documents using
 ;; "chktex" as a backend.  You must be running Emacs 26 or newer.
 ;; Enable it by adding the following to your init file:
 
-;; (add-hook 'LaTeX-mode-hook flymake-mode)
+;;   (add-hook 'LaTeX-mode-hook #'flymake-mode)
 
 ;;; Code:
 
