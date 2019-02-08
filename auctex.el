7c396a9ffe2441bbcec438d22f99061acba1fce3
Tassilo Horn <tsdh@gnu.org>
2019-02-08 18:49:51 +0100

    Release GNU AUCTeX 12.1.2 (now for real)
    * Version 12.1.2 released.

--- auctex.el
+++ auctex.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2014-2017 Free Software Foundation, Inc.
 
-;; Version: 12.1.1
+;; Version: 12.1.2
 ;; URL: http://www.gnu.org/software/auctex/
 ;; Maintainer: auctex-devel@gnu.org
 ;; Notifications-To: auctex-diffs@gnu.org
