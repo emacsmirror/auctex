083964b00d16e3aaf071399408e9c78443d5466a
Arash Esbati <arash@gnu.org>
2019-02-08 21:45:40 +0100

    Delete auctex-pkg.el which is not needed for ELPA releases

--- auctex-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t -*-
-
-(define-package "auctex" "12.1.1" "Integrated environment for *TeX*"
-  '((emacs "24.1") (cl-lib "0.5"))
-  :url "https://www.gnu.org/software/auctex/"
-  :keywords '("TeX" "LaTeX" "Texinfo" "ConTeXt" "docTeX"
-	      "preview-latex"))
-
-(setq byte-compile-warnings '(not unresolved))
