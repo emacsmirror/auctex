f571914d3e627f12987669b7cce107d4f255363f
Tassilo Horn <tsdh@gnu.org>
2020-04-11 21:05:11 +0200

    Compile after merge from master

--- tex-site.el
+++ tex-site.el
@@ -157,11 +157,11 @@ set it with `TeX-modes-set'."
 		       `(TeX-modes-set ',var ,var t))
 		     (setq list (cdr list)))))) )
 
-(defconst AUCTeX-version "12.2.0"
+(defconst AUCTeX-version "12.2.0.2020-04-09"
     "AUCTeX version.
 If not a regular release, the date of the last change.")
 
-(defconst AUCTeX-date "2019-10-31"
+(defconst AUCTeX-date "2020-04-09"
   "AUCTeX release date using the ISO 8601 format, yyyy-mm-dd.")
 
 ;; Store bibitems when saving a BibTeX buffer
