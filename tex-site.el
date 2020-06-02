1006b896038e590337a766fe35777c34c021f18f
Tassilo Horn <tsdh@gnu.org>
2020-06-02 19:57:19 +0200

    Release GNU AUCTeX 12.2.3
    * GNU AUCTeX Version 12.2.3 released.

--- tex-site.el
+++ tex-site.el
@@ -157,11 +157,11 @@ set it with `TeX-modes-set'."
 		       `(TeX-modes-set ',var ,var t))
 		     (setq list (cdr list)))))) )
 
-(defconst AUCTeX-version "12.2.2.2020-05-29"
+(defconst AUCTeX-version "12.2.3"
     "AUCTeX version.
 If not a regular release, the date of the last change.")
 
-(defconst AUCTeX-date "2020-05-29"
+(defconst AUCTeX-date "2020-06-02"
   "AUCTeX release date using the ISO 8601 format, yyyy-mm-dd.")
 
 ;; Store bibitems when saving a BibTeX buffer
