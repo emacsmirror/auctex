1e8a495a4a2cc7376400686816924c0d4e320312
Tassilo Horn <tsdh@gnu.org>
2020-06-27 09:17:19 +0200

    Regenerate docs and tex-site.el after merge

--- tex-site.el
+++ tex-site.el
@@ -157,11 +157,11 @@ set it with `TeX-modes-set'."
 		       `(TeX-modes-set ',var ,var t))
 		     (setq list (cdr list)))))) )
 
-(defconst AUCTeX-version "12.2.3"
+(defconst AUCTeX-version "12.2.3.2020-06-27"
     "AUCTeX version.
 If not a regular release, the date of the last change.")
 
-(defconst AUCTeX-date "2020-06-02"
+(defconst AUCTeX-date "2020-06-27"
   "AUCTeX release date using the ISO 8601 format, yyyy-mm-dd.")
 
 ;; Store bibitems when saving a BibTeX buffer
