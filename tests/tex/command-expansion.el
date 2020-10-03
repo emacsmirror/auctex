c4583f0469a5536183a4018f802de5a75013991c
Ikumi Keita <ikumi@ikumi.que.jp>
2020-10-04 02:34:31 +0900

    ; * tests/tex/command-expansion.el: Update copyright year.

--- tests/tex/command-expansion.el
+++ tests/tex/command-expansion.el
@@ -1,6 +1,6 @@
 ;;; command-expansion.el --- tests for TeX command expansion
 
-;; Copyright (C) 2014, 2018 Free Software Foundation, Inc.
+;; Copyright (C) 2014, 2018, 2020 Free Software Foundation, Inc.
 
 ;; This file is part of AUCTeX.
 
