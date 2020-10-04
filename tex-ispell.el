c3b76ca136f22b298ca66d0838b7b7cf1a310111
Arash Esbati <arash@gnu.org>
2020-10-04 16:10:45 +0200

    * tex-ispell.el: Enable lexical-binding.

--- tex-ispell.el
+++ tex-ispell.el
@@ -1,6 +1,6 @@
-;;; tex-ispell.el --- AUCTeX skip additions for Ispell
+;;; tex-ispell.el --- AUCTeX skip additions for Ispell  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2016--2018 Free Software Foundation, Inc.
+;; Copyright (C) 2016--2020 Free Software Foundation, Inc.
 
 ;; Author: Arash Esbati <arash@gnu.org>
 ;; Maintainer: auctex-devel@gnu.org
