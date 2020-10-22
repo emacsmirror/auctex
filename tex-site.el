a18be78c966888675778655616e2d8f11055d803
Tassilo Horn <tsdh@gnu.org>
2020-10-22 20:07:16 +0200

    Release GNU AUCTeX 12.3.1
    * GNU AUCTeX Version 12.3.1 released.

--- tex-site.el
+++ tex-site.el
@@ -157,7 +157,7 @@ set it with `TeX-modes-set'."
 		       `(TeX-modes-set ',var ,var t))
 		     (setq list (cdr list)))))) )
 
-(defconst AUCTeX-version "12.2.5.2020-10-22"
+(defconst AUCTeX-version "12.3.1"
     "AUCTeX version.
 If not a regular release, the date of the last change.")
 
