emacsmirror/auctex

commit 98455d05f0acf1630dea7a18f5f0b360323c58f1

Give explicit arg to LaTeX-back-to-indentation in filling functions.
Also add some comment filling tests, and adapt one existing test to
new filling behavior introduced in previous commit.

* latex.el (LaTeX-fill-region-as-para-do):
(LaTeX-fill-move-to-break-point): Provide explicit arg to
`LaTeX-back-to-indentation'.

latex.el
@@ -3929,7 +3929,7 @@ space does not end a sentence, so don't break a line there."
         (goto-char from)
         (let* (linebeg
                (code-comment-start (save-excursion
-                                     (LaTeX-back-to-indentation)
+                                     (LaTeX-back-to-indentation 'inner)
                                      (TeX-search-forward-comment-start
                                       (line-end-position))))
                (end-marker (save-excursion
@@ -4040,7 +4040,7 @@ space does not end a sentence, so don't break a line there."
              (TeX-looking-at-backward (concat (regexp-quote TeX-esc) TeX-token-char "*")
                                       (1- (- (point) linebeg)))
              (not (TeX-escaped-p (match-beginning 0))))
-      (goto-char (match-beginning 0)))
+    (goto-char (match-beginning 0)))
   ;; Cater for \verb|...| (and similar) contructs which should not be
   ;; broken. (FIXME: Make it work with shortvrb.sty (also loaded by
   ;; doc.sty) where |...| is allowed.  Arbitrary delimiters may be
@@ -4081,7 +4081,7 @@ space does not end a sentence, so don't break a line there."
           start-point)
       (save-excursion
         (beginning-of-line)
-        (LaTeX-back-to-indentation)
+        (LaTeX-back-to-indentation 'outer)
         (setq start-point (point))
         ;; Find occurences of [, $, {, }, \(, \), \[, \] or $$.
         (while (and (= final-breakpoint orig-breakpoint)

tests/latex/latex-comment-filling-in.tex
@@ -1,0 +1,35 @@
+% This is a very long comment which should be wrapped around after fill-column characters.  We'll see if that works.
+
+% \begin{itemize}
+% \item This is a long item text which should be wrapped around after fill-column characters even though it is commented.
+% \item This is short.
+%   This also.
+%   So all 3 gonna be joined.
+% \end{itemize}
+
+\begin{quote}
+  % This is a very long comment which should be wrapped around after fill-column characters.  We'll see if that works.  
+\end{quote}
+
+\begin{quote}
+  % \begin{itemize}
+  % \item This is a long item text which should be wrapped around after fill-column characters even though it is commented.  This should still work although we're already indented.
+  % \item This is short.
+  %   This also.
+  %   So all 3 gonna be joined.
+  % \end{itemize}  
+\end{quote}
+
+% This is short.\\
+% But no join because the backslashes.
+
+% This is short.\par
+% But no join because the par.
+
+% Code comments.
+
+\foo\bar % This is a code comment.
+\baz     % This also.
+\quux    % So lines must not be joined.
+\foobar  % But very long lines, such as this one, well, you know, they may be wrapped around.  TODO: It would be cool if this was indented as much as the previous line.
+\bar     % This is short.

tests/latex/latex-comment-filling-out.tex
@@ -1,0 +1,38 @@
+% This is a very long comment which should be wrapped around after
+% fill-column characters.  We'll see if that works.
+
+% \begin{itemize}
+% \item This is a long item text which should be wrapped around after
+%   fill-column characters even though it is commented.
+% \item This is short.  This also.  So all 3 gonna be joined.
+% \end{itemize}
+
+\begin{quote}
+  % This is a very long comment which should be wrapped around after
+  % fill-column characters.  We'll see if that works.
+\end{quote}
+
+\begin{quote}
+  % \begin{itemize}
+  % \item This is a long item text which should be wrapped around
+  %   after fill-column characters even though it is commented.  This
+  %   should still work although we're already indented.
+  % \item This is short.  This also.  So all 3 gonna be joined.
+  % \end{itemize}
+\end{quote}
+
+% This is short.\\
+% But no join because the backslashes.
+
+% This is short.\par
+% But no join because the par.
+
+% Code comments.
+
+\foo\bar % This is a code comment.
+\baz     % This also.
+\quux    % So lines must not be joined.
+\foobar  % But very long lines, such as this one, well, you know, they
+% may be wrapped around.  TODO: It would be cool if this was indented
+% as much as the previous line.
+\bar     % This is short.

tests/latex/latex-test.el
@@ -33,6 +33,10 @@
  "latex-filling-in.tex"
  'LaTeX-filling/out
  "latex-filling-out.tex"
+ 'LaTeX-comment-filling/in
+ "latex-comment-filling-in.tex"
+ 'LaTeX-comment-filling/out
+ "latex-comment-filling-out.tex"
  'LaTeX-math-indent/in
  "math-indent-in.tex"
  'LaTeX-math-indent/out
@@ -94,6 +98,27 @@
              (buffer-string))
            (with-temp-buffer
              (insert-file-contents LaTeX-filling/out)
+             (buffer-string)))))
+
+;; Test for comment filling, especially with
+;; `LaTeX-syntactic-comments' which is t by default.
+(ert-deftest LaTeX-comment-filling ()
+  (should (string=
+           (with-temp-buffer
+             (insert-file-contents LaTeX-comment-filling/in)
+             (LaTeX-mode)
+             (let ((fill-column 70)
+                   (code-comment-test nil))
+               (fill-paragraph)
+               (while (= 0 (forward-line 1))
+                 (when (looking-at "% Code comments.")
+                   (setq code-comment-test t))
+                 (when code-comment-test
+                   (LaTeX-back-to-indentation 'inner))
+                 (fill-paragraph)))
+             (buffer-string))
+           (with-temp-buffer
+             (insert-file-contents LaTeX-comment-filling/out)
              (buffer-string)))))
 
 ;; Test for bug#19281 (https://debbugs.gnu.org/cgi/bugreport.cgi?bug=19281):
@@ -453,9 +478,7 @@ ghi"))
                (buffer-string)
                "\\begin{quote}
   % \\begin{center}
-  %   abc
-  %   def
-  %   ghi
+  %   abc def ghi
   % \\end{center}
 \\end{quote}
 "))
@@ -478,9 +501,7 @@ ghi"))
                (buffer-string)
                "\\begin{quote}
   % \\begin{center}
-  %   abc
-  %   def
-  %   ghi
+  %   abc def ghi
   % \\end{center}
 \\end{quote}
 "))
@@ -504,9 +525,7 @@ ghi"))
                (buffer-string)
                "\\begin{quote}
   % \\begin{center}
-  %   abc
-  %   def
-  %   ghi
+  %   abc def ghi
   % \\end{center}
 \\end{quote}
 "))
@@ -529,9 +548,7 @@ ghi"))
                (buffer-string)
                "\\begin{quote}
   \\begin{center}
-    % abc
-    % def
-    % ghi
+    % abc def ghi
   \\end{center}
 \\end{quote}
 ")))))

commit 0a87fe0a0a63b4bf26cec1af0cc372304e548c6c

Fix filling issue caused by commit 98455d05.
* latex.el (LaTeX-fill-region-as-para-do): Call
LaTeX-back-to-indentation without explicit arg again.

latex.el
@@ -3929,7 +3929,7 @@ space does not end a sentence, so don't break a line there."
         (goto-char from)
         (let* (linebeg
                (code-comment-start (save-excursion
-                                     (LaTeX-back-to-indentation 'inner)
+                                     (LaTeX-back-to-indentation)
                                      (TeX-search-forward-comment-start
                                       (line-end-position))))
                (end-marker (save-excursion