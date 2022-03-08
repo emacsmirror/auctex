36655f6a5ed9ffc6552e1cab6f7a8b021b8ad7ca
Ikumi Keita <ikumi@ikumi.que.jp>
2022-03-08 18:08:01 +0900

    Revert "Fix simultaneity"
    This reverts commit e032df90e792ef9b85be3e8e3528c947ecdb5b6a
    because it breaks region preview.
    
    * tex-buf.el (TeX-current-process-region-p):
    (TeX-command, TeX-command-sentinel):
    Revert.

--- tex-buf.el
+++ tex-buf.el
@@ -285,6 +285,7 @@ at bottom if LINE is nil."
 ;; far down (i.e. further down than their first use), so we have to pre-declare
 ;; them here to explain it to the compiler.
 ;; We should move those vars's definitions earlier instead!
+(defvar TeX-current-process-region-p)
 (defvar TeX-save-query)
 (defvar TeX-parse-function)
 (defvar TeX-sentinel-function)
@@ -486,11 +487,6 @@ Do you want to select one of these engines? "
   :group 'TeX-command
   :type 'integer)
 
-(defvar TeX-current-process-region-p nil
-  "Non-nil means that the last TeX command is on a region.")
-(defvar-local TeX--this-process-region-flag nil
-  "Per process value of `TeX-current-process-region-p'.")
-
 (defun TeX-command (name file-fn &optional override-confirm)
   "Run command NAME on the file returned by calling FILE-FN.
 
@@ -560,9 +556,7 @@ remember to add /Library/TeX/texbin/ to your PATH"
     ;; Now start the process
     (let ((file (funcall file-fn)))
       (TeX-process-set-variable file 'TeX-command-next TeX-command-Show)
-      (funcall hook name command file)
-      (TeX-process-set-variable file 'TeX--this-process-region-flag
-                                TeX-current-process-region-p))))
+      (funcall hook name command file))))
 
 (defun TeX-command-expand (command &optional list)
   "Expand COMMAND for `TeX-active-master' as described in LIST.
@@ -1459,8 +1453,8 @@ Insert MSG with some additional information."
              (TeX-command-mode-line process)
              (setq TeX-command-next TeX-command-Show)
              (goto-char (point-min))
-             (let ((TeX-current-process-region-p TeX--this-process-region-flag))
-               (funcall TeX-sentinel-function process name))
+             (apply TeX-sentinel-function process name nil)
+
 
              ;; If buffer and mode line will show that the process
              ;; is dead, we can delete it now.  Otherwise it
@@ -2033,6 +2027,9 @@ command."
 
 
 ;;; Active Process
+
+(defvar TeX-current-process-region-p nil
+  "Non-nil means that the last TeX command is on a region.")
 
 (defun TeX-active-process ()
   "Return the active process for the current buffer."
