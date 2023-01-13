b3d4dfd77833af015befa31c767688ae6432a84b
Tassilo Horn <tsdh@gnu.org>
2023-01-13 20:36:14 +0100

    Release GNU AUCTeX 13.1.7
    * GNU AUCTeX Version 13.1.7 released.

--- tex-site.el
+++ tex-site.el
@@ -126,11 +126,11 @@ set it with `TeX-modes-set'."
   :set #'TeX-modes-set
   :initialize #'custom-initialize-reset)
 
-(defconst AUCTeX-version "13.1.6"
+(defconst AUCTeX-version "13.1.7"
     "AUCTeX version.
 If not a regular release, the date of the last change.")
 
-(defconst AUCTeX-date "2022-11-18"
+(defconst AUCTeX-date "2023-01-13"
   "AUCTeX release date using the ISO 8601 format, yyyy-mm-dd.")
 
 ;; Store bibitems when saving a BibTeX buffer
