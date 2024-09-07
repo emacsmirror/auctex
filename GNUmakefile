9f9a2964b1ea1f7ec78a9ff9fae3824e88e4d9f1
Arash Esbati <arash@gnu.org>
2024-09-07 22:58:10 +0200

    Adjust license notice
    ELPA packages are regarded as part of Emacs and are distributed
    under the same license.  Change license note accordingly.  See:
    https://lists.gnu.org/archive/html/emacs-devel/2024-09/msg00246.html

--- GNUmakefile
+++ GNUmakefile
@@ -1,6 +1,21 @@
 # Rules to generate the files that need to go into the ELPA package.
 
-# Files we need to auto-generate for elpa:
+# This file is part of GNU Emacs.
+
+# GNU Emacs is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any
+# later version.
+
+# GNU Emacs is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+# Files we need to auto-generate for ELPA:
 #   README
 #   ChangeLog
 #   doc: preview-dtxdoc.texi
--- style/overpic.el
+++ style/overpic.el
@@ -7,22 +7,20 @@
 ;; Created: 2020-02-23
 ;; Keywords: tex
 
-;; This file is part of AUCTeX.
+;; This file is part of GNU Emacs.
 
-;; AUCTeX is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; GNU Emacs is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation; either version 3, or (at your option) any
+;; later version.
 
-;; AUCTeX is distributed in the hope that it will be useful, but
+;; GNU Emacs is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with AUCTeX; see the file COPYING.  If not, write to the Free
-;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-;; 02110-1301, USA.
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
