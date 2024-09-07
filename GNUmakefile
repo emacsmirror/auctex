86fa24978ca35c0e5c797f9bac27c75cc00897c6
Arash Esbati <arash@gnu.org>
2024-09-07 23:23:11 +0200

    Revert "Adjust license notice"
    This reverts commit 9f9a2964b1ea1f7ec78a9ff9fae3824e88e4d9f1.
    
    Revert change after this comment:
    https://lists.gnu.org/archive/html/emacs-devel/2024-09/msg00251.html

--- GNUmakefile
+++ GNUmakefile
@@ -1,21 +1,6 @@
 # Rules to generate the files that need to go into the ELPA package.
 
-# This file is part of GNU Emacs.
-
-# GNU Emacs is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation; either version 3, or (at your option) any
-# later version.
-
-# GNU Emacs is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
-
-# Files we need to auto-generate for ELPA:
+# Files we need to auto-generate for elpa:
 #   README
 #   ChangeLog
 #   doc: preview-dtxdoc.texi
--- style/paracol.el
+++ style/paracol.el
@@ -7,20 +7,22 @@
 ;; Created: 2016-05-26
 ;; Keywords: tex
 
-;; This file is part of GNU Emacs.
+;; This file is part of AUCTeX.
 
-;; GNU Emacs is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by the
-;; Free Software Foundation; either version 3, or (at your option) any
-;; later version.
+;; AUCTeX is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful, but
+;; AUCTeX is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;; along with AUCTeX; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
 
 ;;; Commentary:
 
