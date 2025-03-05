cd23c19918edb57e8901326d3e942b10ea271771
Ikumi Keita <ikumi@ikumi.que.jp>
2025-03-06 02:28:46 +0900

    ; * tests/latex/latex-test.el: Update copyright year.

--- tests/latex/latex-test.el
+++ tests/latex/latex-test.el
@@ -1,6 +1,6 @@
 ;;; latex-test.el --- tests for LaTeX mode  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014-2024  Free Software Foundation, Inc.
+;; Copyright (C) 2014-2025  Free Software Foundation, Inc.
 
 ;; This file is part of AUCTeX.
 
