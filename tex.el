8ac3fd74ed9e9df0bedd6bb5bd09e41ab15484b3
Arash Esbati <arash@gnu.org>
2025-03-10 22:02:50 +0100

    Add new style argument hooks
    * doc/auctex.texi (Adding Macros): Document them.
    
    * tex.el (TeX-arg-space, TeX-arg-set-exit-mark): Add new
    functions.

--- tex.el
+++ tex.el
@@ -3688,6 +3688,17 @@ Used for specifying extra syntax for a macro.  The compatibility
 argument OPTIONAL is ignored."
   (apply #'insert args))
 
+(defun TeX-arg-space (_optional &optional how-many)
+  "Ignore OPTIONAL and insert 1 or HOW-MANY spaces.
+
+This function is equivalent to
+  (TeX-arg-literal \" \")
+when HOW-MANY is omitted."
+  (insert (make-string (or how-many 1) ?\s)))
+
+(defun TeX-arg-set-exit-mark (_optional &optional pos)
+  "Ignore OPTIONAL and set `TeX-exit-mark' to POS or current point."
+  (set-marker TeX-exit-mark (or pos (point))))
 
 ;;; Font Locking
 
