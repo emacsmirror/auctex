001686a66066774c8d3d3541c09ac735aef62455
Arash Esbati <arash@gnu.org>
2026-01-12 13:24:59 +0100

    Release GNU AUCTeX 14.1.1
    * GNU AUCTeX Version 14.1.1 released.

--- auctex.el
+++ auctex.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2014-2025 Free Software Foundation, Inc.
 
-;; Version: 14.1.0
+;; Version: 14.1.1
 ;; URL: https://www.gnu.org/software/auctex/
 ;; Maintainer: auctex-devel@gnu.org
 ;; Notifications-To: auctex-diffs@gnu.org
